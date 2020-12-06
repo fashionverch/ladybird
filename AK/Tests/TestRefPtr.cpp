fashionverch/ladybird

commit 824d2d3a500012106b1b4f9b610f81a785007164

AK: Allow having ref counted pointers to const object
We allow the ref-counting parts of an object to be mutated even when the
object itself is a const.

An important detail is that we allow invoking 'will_be_destroyed' and
'one_ref_left', which are not required to be const qualified, on const
objects.

AK/RefCounted.h
@@ -32,9 +32,9 @@
 namespace AK {
 
 template<class T>
-constexpr auto call_will_be_destroyed_if_present(T* object) -> decltype(object->will_be_destroyed(), TrueType {})
+constexpr auto call_will_be_destroyed_if_present(const T* object) -> decltype(object->will_be_destroyed(), TrueType {})
 {
-    object->will_be_destroyed();
+    const_cast<T*>(object)->will_be_destroyed();
     return {};
 }
 
@@ -44,9 +44,9 @@ constexpr auto call_will_be_destroyed_if_present(...) -> FalseType
 }
 
 template<class T>
-constexpr auto call_one_ref_left_if_present(T* object) -> decltype(object->one_ref_left(), TrueType {})
+constexpr auto call_one_ref_left_if_present(const T* object) -> decltype(object->one_ref_left(), TrueType {})
 {
-    object->one_ref_left();
+    const_cast<T*>(object)->one_ref_left();
     return {};
 }
 
@@ -57,7 +57,7 @@ constexpr auto call_one_ref_left_if_present(...) -> FalseType
 
 class RefCountedBase {
 public:
-    void ref()
+    void ref() const
     {
         ASSERT(m_ref_count);
         ++m_ref_count;
@@ -75,26 +75,26 @@ protected:
         ASSERT(!m_ref_count);
     }
 
-    void deref_base()
+    void deref_base() const
     {
         ASSERT(m_ref_count);
         --m_ref_count;
     }
 
-    int m_ref_count { 1 };
+    mutable int m_ref_count { 1 };
 };
 
 template<typename T>
 class RefCounted : public RefCountedBase {
 public:
-    void unref()
+    void unref() const
     {
         deref_base();
         if (m_ref_count == 0) {
-            call_will_be_destroyed_if_present(static_cast<T*>(this));
-            delete static_cast<T*>(this);
+            call_will_be_destroyed_if_present(static_cast<const T*>(this));
+            delete static_cast<const T*>(this);
         } else if (m_ref_count == 1) {
-            call_one_ref_left_if_present(static_cast<T*>(this));
+            call_one_ref_left_if_present(static_cast<const T*>(this));
         }
     }
 };

commit b47697828763f3c4e75c9a258c217b37554c3e60

AK: Add test for the will_be_destroyed and one_ref_left magic functions
Fixes a regression introduced by 824d2d3. The commit description there
asserts that the changes allow calling will_be_destroyed and
one_ref_left, which are not required to be const qualified. The
implementation in fact does require the methods to be const qualified,
because we forgot to add the const_cast inside the decltypes :^)

AK/RefCounted.h
@@ -36,7 +36,7 @@
 namespace AK {
 
 template<class T>
-constexpr auto call_will_be_destroyed_if_present(const T* object) -> decltype(object->will_be_destroyed(), TrueType {})
+constexpr auto call_will_be_destroyed_if_present(const T* object) -> decltype(const_cast<T*>(object)->will_be_destroyed(), TrueType {})
 {
     const_cast<T*>(object)->will_be_destroyed();
     return {};
@@ -48,7 +48,7 @@ constexpr auto call_will_be_destroyed_if_present(...) -> FalseType
 }
 
 template<class T>
-constexpr auto call_one_ref_left_if_present(const T* object) -> decltype(object->one_ref_left(), TrueType {})
+constexpr auto call_one_ref_left_if_present(const T* object) -> decltype(const_cast<T*>(object)->one_ref_left(), TrueType {})
 {
     const_cast<T*>(object)->one_ref_left();
     return {};

AK/Tests/TestRefPtr.cpp
@@ -36,6 +36,15 @@ struct Object : public RefCounted<Object> {
 struct Object2 : Object {
 };
 
+struct SelfAwareObject : public RefCounted<SelfAwareObject> {
+    void one_ref_left() { m_has_one_ref_left = true; }
+    void will_be_destroyed() { ++num_destroyed; }
+
+    bool m_has_one_ref_left = false;
+    static size_t num_destroyed;
+};
+size_t SelfAwareObject::num_destroyed = 0;
+
 TEST_CASE(basics)
 {
     RefPtr<Object> object = adopt(*new Object);
@@ -129,6 +138,27 @@ TEST_CASE(assign_copy_self)
 #endif
 
     EXPECT_EQ(object->ref_count(), 1u);
+}
+
+TEST_CASE(self_observers)
+{
+    RefPtr<SelfAwareObject> object = adopt(*new SelfAwareObject);
+    EXPECT_EQ(object->ref_count(), 1u);
+    EXPECT_EQ(object->m_has_one_ref_left, false);
+    EXPECT_EQ(SelfAwareObject::num_destroyed, 0u);
+
+    object->ref();
+    EXPECT_EQ(object->ref_count(), 2u);
+    EXPECT_EQ(object->m_has_one_ref_left, false);
+    EXPECT_EQ(SelfAwareObject::num_destroyed, 0u);
+
+    object->unref();
+    EXPECT_EQ(object->ref_count(), 1u);
+    EXPECT_EQ(object->m_has_one_ref_left, true);
+    EXPECT_EQ(SelfAwareObject::num_destroyed, 0u);
+
+    object->unref();
+    EXPECT_EQ(SelfAwareObject::num_destroyed, 1u);
 }
 
 TEST_MAIN(RefPtr)