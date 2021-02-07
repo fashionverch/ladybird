fashionverch/ladybird

commit 62d1e0574bcc568e57f003503012b7e6fa54da12

LibGfx: Don't reject valid GIF animations with interlaced frames
We were returning early from the deinterlacing loop after the very last
pass, but we should just let the outer loop finish and return instead.

This makes the Netscape animation on https://timmorgan.dev work. :^)

Userland/Libraries/LibGfx/GIFLoader.cpp
@@ -401,10 +401,8 @@ static bool decode_frame(GIFLoadingContext& context, size_t frame_index)
                     if (image.interlaced) {
                         if (row + INTERLACE_ROW_STRIDES[interlace_pass] >= image.height) {
                             ++interlace_pass;
-                            // FIXME: We could probably figure this out earlier and fail before doing a bunch of work.
-                            if (interlace_pass >= 4)
-                                return false;
-                            row = INTERLACE_ROW_OFFSETS[interlace_pass];
+                            if (interlace_pass < 4)
+                                row = INTERLACE_ROW_OFFSETS[interlace_pass];
                         } else {
                             row += INTERLACE_ROW_STRIDES[interlace_pass];
                         }

commit d4ecb96b441cbb47851b6d672eb57725d1e93271

LibGfx: Fix global-buffer-overflow in interlaced GIF decode
Regressed with 62d1e0574bcc568e57f003503012b7e6fa54da12 and immediately
caught by oss-fuzz: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=30507

Userland/Libraries/LibGfx/GIFLoader.cpp
@@ -404,7 +404,8 @@ static bool decode_frame(GIFLoadingContext& context, size_t frame_index)
                             if (interlace_pass < 4)
                                 row = INTERLACE_ROW_OFFSETS[interlace_pass];
                         } else {
-                            row += INTERLACE_ROW_STRIDES[interlace_pass];
+                            if (interlace_pass < 4)
+                                row += INTERLACE_ROW_STRIDES[interlace_pass];
                         }
                     } else {
                         ++row;