fashionverch/ladybird

commit 0ae5ea5b40be9d89cf0817a8205bb286d532900d

LibWeb+Base: Use AK::SourceGenerator for error pages
Instead of storing a format string in a file, let's be reasonable
and use SourceGenerator's template functionality. :^)

Userland/Libraries/LibWeb/Loader/FrameLoader.cpp
@@ -26,6 +26,7 @@
 
 #include <AK/Debug.h>
 #include <AK/LexicalPath.h>
+#include <AK/SourceGenerator.h>
 #include <LibGemini/Document.h>
 #include <LibGfx/ImageDecoder.h>
 #include <LibMarkdown/Document.h>
@@ -226,13 +227,12 @@ void FrameLoader::load_error_page(const URL& failed_url, const String& error)
         error_page_url,
         [this, failed_url, error](auto data, auto&, auto) {
             VERIFY(!data.is_null());
-#pragma GCC diagnostic ignored "-Wformat-nonliteral"
-            auto html = String::formatted(
-                data,
-                escape_html_entities(failed_url.to_string()),
-                escape_html_entities(error));
-#pragma GCC diagnostic pop
-            auto document = HTML::parse_html_document(html, failed_url, "utf-8");
+            StringBuilder builder;
+            SourceGenerator generator { builder };
+            generator.set("failed_url", failed_url.to_string());
+            generator.set("error", error);
+            generator.append(data);
+            auto document = HTML::parse_html_document(generator.as_string_view(), failed_url, "utf-8");
             VERIFY(document);
             frame().set_document(document);
         },

Base/res/html/error.html
@@ -14,8 +14,8 @@
     <body>
         <header>
             <img src="file:///res/icons/32x32/msgbox-warning.png" alt="Warning" width="24" height="24">
-            <h1>Failed to load {}</h1>
+            <h1>Failed to load @failed_url@</h1>
         </header>
-        <p>Error: {}</p>
+        <p>Error: @error@</p>
     </body>
 </html>

commit f037916e16683bcac4bb5cae9ef191bb2306f0c4

LibWeb: Fix HTML injection on FrameLoader error page
Small regression introduced by 0ae5ea5, we still have to escape HTML
entities.

Userland/Libraries/LibWeb/Loader/FrameLoader.cpp
@@ -229,8 +229,8 @@ void FrameLoader::load_error_page(const URL& failed_url, const String& error)
             VERIFY(!data.is_null());
             StringBuilder builder;
             SourceGenerator generator { builder };
-            generator.set("failed_url", failed_url.to_string());
-            generator.set("error", error);
+            generator.set("failed_url", escape_html_entities(failed_url.to_string()));
+            generator.set("error", escape_html_entities(error));
             generator.append(data);
             auto document = HTML::parse_html_document(generator.as_string_view(), failed_url, "utf-8");
             VERIFY(document);