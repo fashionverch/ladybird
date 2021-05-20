fashionverch/ladybird

commit ba939020c03e80de65f6e704dbd1080bb26b37d7

Kernel: Avoid allocating under spinlock in ProcessGroup::find_or_create
Avoid allocating while holding the g_process_groups_lock spinlock, it's
a pattern that has a negative effect on performance and scalability,
especially given that it is a global lock, reachable by all processes.

Kernel/ProcessGroup.cpp
@@ -30,18 +30,31 @@ RefPtr<ProcessGroup> ProcessGroup::create(ProcessGroupID pgid)
 
 RefPtr<ProcessGroup> ProcessGroup::find_or_create(ProcessGroupID pgid)
 {
+    // Avoid allocating under spinlock, this compromises by wasting the
+    // allocation when we race with another process to see if they have
+    // already created a process group.
+    auto process_group = adopt_ref_if_nonnull(new ProcessGroup(pgid));
     ScopedSpinLock lock(g_process_groups_lock);
 
-    if (auto existing = from_pgid(pgid))
+    if (auto existing = from_pgid_nolock(pgid))
         return existing.release_nonnull();
 
-    return create(pgid);
+    if (!process_group)
+        return {};
+
+    g_process_groups->prepend(process_group);
+    return process_group;
 }
 
 RefPtr<ProcessGroup> ProcessGroup::from_pgid(ProcessGroupID pgid)
 {
     ScopedSpinLock lock(g_process_groups_lock);
+    return from_pgid_nolock(pgid);
+}
 
+RefPtr<ProcessGroup> ProcessGroup::from_pgid_nolock(ProcessGroupID pgid)
+{
+    VERIFY(g_process_groups_lock.own_lock());
     for (auto& group : *g_process_groups) {
         if (group.pgid() == pgid)
             return &group;

Kernel/ProcessGroup.h
@@ -40,6 +40,8 @@ private:
     {
     }
 
+    static RefPtr<ProcessGroup> from_pgid_nolock(ProcessGroupID);
+
     ProcessGroup* m_prev { nullptr };
     ProcessGroup* m_next { nullptr };
 

commit 45cacb2ba276277a927e48985fd4ebc2e3c8d1bd

Kernel: Fix regression, removing a ProcessGroup that not in the list
I introduced this bug in ba939020c, where it's possible that the
ProcessGroup is created, but we never add it to the list. Make sure we
check that we are in the list before removal. This only broke booting in
self-test mode oddly enough.

Reported-By: Andrew Kaster <andrewdkaster@gmail.com>

Kernel/ProcessGroup.cpp
@@ -14,7 +14,9 @@ InlineLinkedList<ProcessGroup>* g_process_groups;
 ProcessGroup::~ProcessGroup()
 {
     ScopedSpinLock lock(g_process_groups_lock);
-    g_process_groups->remove(this);
+    if (m_next || m_prev) {
+        g_process_groups->remove(this);
+    }
 }
 
 RefPtr<ProcessGroup> ProcessGroup::create(ProcessGroupID pgid)