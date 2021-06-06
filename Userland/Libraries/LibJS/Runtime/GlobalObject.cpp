fashionverch/ladybird

commit 7191c8957c048d947ecc299c776a80e73fd01013

Everywhere: Replace ctype.h to avoid narrowing conversions
This replaces ctype.h with CharacterType.h everywhere I could find
issues with narrowing conversions. While using it will probably make
sense almost everywhere in the future, the most critical places should
have been addressed.

Userland/Libraries/LibJS/Runtime/GlobalObject.cpp
@@ -5,6 +5,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <AK/CharacterTypes.h>
 #include <AK/Hex.h>
 #include <AK/Platform.h>
 #include <AK/TemporaryChange.h>
@@ -56,7 +57,6 @@
 #include <LibJS/Runtime/TypedArrayConstructor.h>
 #include <LibJS/Runtime/TypedArrayPrototype.h>
 #include <LibJS/Runtime/Value.h>
-#include <ctype.h>
 
 namespace JS {
 
@@ -249,16 +249,10 @@ JS_DEFINE_NATIVE_FUNCTION(GlobalObject::parse_int)
     }
 
     auto parse_digit = [&](u32 code_point, i32 radix) -> Optional<i32> {
-        i32 digit = -1;
-
-        if (isdigit(code_point))
-            digit = code_point - '0';
-        else if (islower(code_point))
-            digit = 10 + (code_point - 'a');
-        else if (isupper(code_point))
-            digit = 10 + (code_point - 'A');
-
-        if (digit == -1 || digit >= radix)
+        if (!is_ascii_hex_digit(code_point) || radix <= 0)
+            return {};
+        auto digit = parse_ascii_hex_digit(code_point);
+        if (digit >= (u32)radix)
             return {};
         return digit;
     };

AK/URL.cpp
@@ -5,6 +5,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <AK/CharacterTypes.h>
 #include <AK/Debug.h>
 #include <AK/LexicalPath.h>
 #include <AK/StringBuilder.h>
@@ -13,26 +14,6 @@
 #include <AK/Utf8View.h>
 
 namespace AK {
-
-constexpr bool is_ascii_alpha(u32 code_point)
-{
-    return ('a' <= code_point && code_point <= 'z') || ('A' <= code_point && code_point <= 'Z');
-}
-
-constexpr bool is_ascii_digit(u32 code_point)
-{
-    return '0' <= code_point && code_point <= '9';
-}
-
-constexpr bool is_ascii_alphanumeric(u32 code_point)
-{
-    return is_ascii_alpha(code_point) || is_ascii_digit(code_point);
-}
-
-constexpr bool is_ascii_hex_digit(u32 code_point)
-{
-    return is_ascii_digit(code_point) || (code_point >= 'a' && code_point <= 'f') || (code_point >= 'A' && code_point <= 'F');
-}
 
 // FIXME: It could make sense to force users of URL to use URLParser::parse() explicitly instead of using a constructor.
 URL::URL(StringView const& string)
@@ -403,17 +384,6 @@ String URL::percent_encode(StringView const& input, URL::PercentEncodeSet set)
     return builder.to_string();
 }
 
-constexpr u8 parse_hex_digit(u8 digit)
-{
-    if (digit >= '0' && digit <= '9')
-        return digit - '0';
-    if (digit >= 'a' && digit <= 'f')
-        return digit - 'a' + 10;
-    if (digit >= 'A' && digit <= 'F')
-        return digit - 'A' + 10;
-    VERIFY_NOT_REACHED();
-}
-
 String URL::percent_decode(StringView const& input)
 {
     if (!input.contains('%'))
@@ -427,9 +397,9 @@ String URL::percent_decode(StringView const& input)
             builder.append_code_point(*it);
         } else {
             ++it;
-            u8 byte = parse_hex_digit(*it) << 4;
+            u8 byte = parse_ascii_hex_digit(*it) << 4;
             ++it;
-            byte += parse_hex_digit(*it);
+            byte += parse_ascii_hex_digit(*it);
             builder.append(byte);
         }
     }

AK/URLParser.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <AK/CharacterTypes.h>
 #include <AK/Debug.h>
 #include <AK/Optional.h>
 #include <AK/SourceLocation.h>
@@ -14,26 +15,6 @@
 #include <AK/Utf8View.h>
 
 namespace AK {
-
-constexpr bool is_ascii_alpha(u32 code_point)
-{
-    return ('a' <= code_point && code_point <= 'z') || ('A' <= code_point && code_point <= 'Z');
-}
-
-constexpr bool is_ascii_digit(u32 code_point)
-{
-    return '0' <= code_point && code_point <= '9';
-}
-
-constexpr bool is_ascii_alphanumeric(u32 code_point)
-{
-    return is_ascii_alpha(code_point) || is_ascii_digit(code_point);
-}
-
-constexpr bool is_ascii_hex_digit(u32 code_point)
-{
-    return is_ascii_digit(code_point) || (code_point >= 'a' && code_point <= 'f') || (code_point >= 'A' && code_point <= 'F');
-}
 
 constexpr bool is_url_code_point(u32 code_point)
 {

Userland/Libraries/LibGUI/EditingEngine.cpp
@@ -4,11 +4,22 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <AK/CharacterTypes.h>
 #include <LibGUI/EditingEngine.h>
 #include <LibGUI/Event.h>
 #include <LibGUI/TextEditor.h>
 
 namespace GUI {
+
+constexpr bool is_vim_alphanumeric(u32 code_point)
+{
+    return is_ascii_alphanumeric(code_point) || code_point == '_';
+}
+
+constexpr bool is_vim_punctuation(u32 code_point)
+{
+    return is_ascii_punctuation(code_point) && code_point != '_';
+}
 
 EditingEngine::~EditingEngine()
 {
@@ -379,15 +390,7 @@ TextPosition EditingEngine::find_beginning_of_next_word()
      * If the end of the input is reached, jump there
      */
 
-    auto vim_isalnum = [](int c) {
-        return c == '_' || isalnum(c);
-    };
-
-    auto vim_ispunct = [](int c) {
-        return c != '_' && ispunct(c);
-    };
-
-    bool started_on_punct = vim_ispunct(m_editor->current_line().to_utf8().characters()[m_editor->cursor().column()]);
+    bool started_on_punct = is_vim_punctuation(m_editor->current_line().to_utf8().characters()[m_editor->cursor().column()]);
     bool has_seen_whitespace = false;
     bool is_first_line = true;
     auto& lines = m_editor->lines();
@@ -409,18 +412,18 @@ TextPosition EditingEngine::find_beginning_of_next_word()
             const u32* line_chars = line.view().code_points();
             const u32 current_char = line_chars[column_index];
 
-            if (started_on_punct && vim_isalnum(current_char)) {
+            if (started_on_punct && is_vim_alphanumeric(current_char)) {
                 return { line_index, column_index };
             }
 
-            if (vim_ispunct(current_char) && !started_on_punct) {
+            if (is_vim_punctuation(current_char) && !started_on_punct) {
                 return { line_index, column_index };
             }
 
-            if (isspace(current_char))
+            if (is_ascii_space(current_char))
                 has_seen_whitespace = true;
 
-            if (has_seen_whitespace && (vim_isalnum(current_char) || vim_ispunct(current_char))) {
+            if (has_seen_whitespace && (is_vim_alphanumeric(current_char) || is_vim_punctuation(current_char))) {
                 return { line_index, column_index };
             }
 
@@ -449,14 +452,6 @@ TextPosition EditingEngine::find_end_of_next_word()
      * If the end of the input is reached, jump there
      */
 
-    auto vim_isalnum = [](int c) {
-        return c == '_' || isalnum(c);
-    };
-
-    auto vim_ispunct = [](int c) {
-        return c != '_' && ispunct(c);
-    };
-
     bool is_first_line = true;
     bool is_first_iteration = true;
     auto& lines = m_editor->lines();
@@ -481,7 +476,7 @@ TextPosition EditingEngine::find_end_of_next_word()
             const u32* line_chars = line.view().code_points();
             const u32 current_char = line_chars[column_index];
 
-            if (column_index == lines.at(line_index).length() - 1 && !is_first_iteration && (vim_isalnum(current_char) || vim_ispunct(current_char)))
+            if (column_index == lines.at(line_index).length() - 1 && !is_first_iteration && (is_vim_alphanumeric(current_char) || is_vim_punctuation(current_char)))
                 return { line_index, column_index };
             else if (column_index == lines.at(line_index).length() - 1) {
                 is_first_iteration = false;
@@ -490,10 +485,10 @@ TextPosition EditingEngine::find_end_of_next_word()
 
             const u32 next_char = line_chars[column_index + 1];
 
-            if (!is_first_iteration && vim_isalnum(current_char) && (isspace(next_char) || vim_ispunct(next_char)))
+            if (!is_first_iteration && is_vim_alphanumeric(current_char) && (is_ascii_space(next_char) || is_vim_punctuation(next_char)))
                 return { line_index, column_index };
 
-            if (!is_first_iteration && vim_ispunct(current_char) && (isspace(next_char) || vim_isalnum(next_char)))
+            if (!is_first_iteration && is_vim_punctuation(current_char) && (is_ascii_space(next_char) || is_vim_alphanumeric(next_char)))
                 return { line_index, column_index };
 
             if (line_index == lines.size() - 1 && column_index == line.length() - 1) {
@@ -513,15 +508,7 @@ void EditingEngine::move_to_end_of_next_word()
 
 TextPosition EditingEngine::find_end_of_previous_word()
 {
-    auto vim_isalnum = [](int c) {
-        return c == '_' || isalnum(c);
-    };
-
-    auto vim_ispunct = [](int c) {
-        return c != '_' && ispunct(c);
-    };
-
-    bool started_on_punct = vim_ispunct(m_editor->current_line().to_utf8().characters()[m_editor->cursor().column()]);
+    bool started_on_punct = is_vim_punctuation(m_editor->current_line().to_utf8().characters()[m_editor->cursor().column()]);
     bool is_first_line = true;
     bool has_seen_whitespace = false;
     auto& lines = m_editor->lines();
@@ -545,19 +532,19 @@ TextPosition EditingEngine::find_end_of_previous_word()
             const u32* line_chars = line.view().code_points();
             const u32 current_char = line_chars[column_index];
 
-            if (started_on_punct && vim_isalnum(current_char)) {
+            if (started_on_punct && is_vim_alphanumeric(current_char)) {
                 return { line_index, column_index };
             }
 
-            if (vim_ispunct(current_char) && !started_on_punct) {
+            if (is_vim_punctuation(current_char) && !started_on_punct) {
                 return { line_index, column_index };
             }
 
-            if (isspace(current_char)) {
+            if (is_ascii_space(current_char)) {
                 has_seen_whitespace = true;
             }
 
-            if (has_seen_whitespace && (vim_isalnum(current_char) || vim_ispunct(current_char))) {
+            if (has_seen_whitespace && (is_vim_alphanumeric(current_char) || is_vim_punctuation(current_char))) {
                 return { line_index, column_index };
             }
 
@@ -580,14 +567,6 @@ void EditingEngine::move_to_end_of_previous_word()
 
 TextPosition EditingEngine::find_beginning_of_previous_word()
 {
-    auto vim_isalnum = [](int c) {
-        return c == '_' || isalnum(c);
-    };
-
-    auto vim_ispunct = [](int c) {
-        return c != '_' && ispunct(c);
-    };
-
     bool is_first_iterated_line = true;
     bool is_first_iteration = true;
     auto& lines = m_editor->lines();
@@ -618,7 +597,7 @@ TextPosition EditingEngine::find_beginning_of_previous_word()
             const u32* line_chars = line.view().code_points();
             const u32 current_char = line_chars[column_index];
 
-            if (column_index == 0 && !is_first_iteration && (vim_isalnum(current_char) || vim_ispunct(current_char))) {
+            if (column_index == 0 && !is_first_iteration && (is_vim_alphanumeric(current_char) || is_vim_punctuation(current_char))) {
                 return { line_index, column_index };
             } else if (line_index == 0 && column_index == 0) {
                 return { line_index, column_index };
@@ -629,10 +608,10 @@ TextPosition EditingEngine::find_beginning_of_previous_word()
 
             const u32 next_char = line_chars[column_index - 1];
 
-            if (!is_first_iteration && vim_isalnum(current_char) && (isspace(next_char) || vim_ispunct(next_char)))
+            if (!is_first_iteration && is_vim_alphanumeric(current_char) && (is_ascii_space(next_char) || is_vim_punctuation(next_char)))
                 return { line_index, column_index };
 
-            if (!is_first_iteration && vim_ispunct(current_char) && (isspace(next_char) || vim_isalnum(next_char)))
+            if (!is_first_iteration && is_vim_punctuation(current_char) && (is_ascii_space(next_char) || is_vim_alphanumeric(next_char)))
                 return { line_index, column_index };
 
             is_first_iteration = false;

Userland/Libraries/LibGUI/TextDocument.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <AK/Badge.h>
+#include <AK/CharacterTypes.h>
 #include <AK/ScopeGuard.h>
 #include <AK/StringBuilder.h>
 #include <AK/Utf8View.h>
@@ -12,7 +13,6 @@
 #include <LibGUI/TextDocument.h>
 #include <LibGUI/TextEditor.h>
 #include <LibRegex/Regex.h>
-#include <ctype.h>
 
 namespace GUI {
 
@@ -104,7 +104,7 @@ size_t TextDocumentLine::first_non_whitespace_column() const
 {
     for (size_t i = 0; i < length(); ++i) {
         auto code_point = code_points()[i];
-        if (!isspace(code_point))
+        if (!is_ascii_space(code_point))
             return i;
     }
     return length();
@@ -114,7 +114,7 @@ Optional<size_t> TextDocumentLine::last_non_whitespace_column() const
 {
     for (ssize_t i = length() - 1; i >= 0; --i) {
         auto code_point = code_points()[i];
-        if (!isspace(code_point))
+        if (!is_ascii_space(code_point))
             return i;
     }
     return {};
@@ -124,7 +124,7 @@ bool TextDocumentLine::ends_in_whitespace() const
 {
     if (!length())
         return false;
-    return isspace(code_points()[length() - 1]);
+    return is_ascii_space(code_points()[length() - 1]);
 }
 
 bool TextDocumentLine::can_select() const
@@ -638,11 +638,11 @@ TextPosition TextDocument::first_word_break_before(const TextPosition& position,
     if (target.column() == line.length())
         modifier = 1;
 
-    auto is_start_alphanumeric = isalnum(line.code_points()[target.column() - modifier]);
+    auto is_start_alphanumeric = is_ascii_alphanumeric(line.code_points()[target.column() - modifier]);
 
     while (target.column() > 0) {
         auto prev_code_point = line.code_points()[target.column() - 1];
-        if ((is_start_alphanumeric && !isalnum(prev_code_point)) || (!is_start_alphanumeric && isalnum(prev_code_point)))
+        if ((is_start_alphanumeric && !is_ascii_alphanumeric(prev_code_point)) || (!is_start_alphanumeric && is_ascii_alphanumeric(prev_code_point)))
             break;
         target.set_column(target.column() - 1);
     }
@@ -662,11 +662,11 @@ TextPosition TextDocument::first_word_break_after(const TextPosition& position) 
         return TextPosition(position.line() + 1, 0);
     }
 
-    auto is_start_alphanumeric = isalnum(line.code_points()[target.column()]);
+    auto is_start_alphanumeric = is_ascii_alphanumeric(line.code_points()[target.column()]);
 
     while (target.column() < line.length()) {
         auto next_code_point = line.code_points()[target.column()];
-        if ((is_start_alphanumeric && !isalnum(next_code_point)) || (!is_start_alphanumeric && isalnum(next_code_point)))
+        if ((is_start_alphanumeric && !is_ascii_alphanumeric(next_code_point)) || (!is_start_alphanumeric && is_ascii_alphanumeric(next_code_point)))
             break;
         target.set_column(target.column() + 1);
     }

Userland/Libraries/LibGUI/TextEditor.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <AK/CharacterTypes.h>
 #include <AK/Debug.h>
 #include <AK/ScopeGuard.h>
 #include <AK/StringBuilder.h>
@@ -25,7 +26,6 @@
 #include <LibGfx/FontDatabase.h>
 #include <LibGfx/Palette.h>
 #include <LibSyntax/Highlighter.h>
-#include <ctype.h>
 #include <fcntl.h>
 #include <math.h>
 #include <stdio.h>
@@ -1232,12 +1232,12 @@ size_t TextEditor::number_of_selected_words() const
     bool in_word = false;
     auto selected_text = this->selected_text();
     for (char c : selected_text) {
-        if (in_word && isspace(c)) {
+        if (in_word && is_ascii_space(c)) {
             in_word = false;
             word_count++;
             continue;
         }
-        if (!in_word && !isspace(c))
+        if (!in_word && !is_ascii_space(c))
             in_word = true;
     }
     if (in_word)
@@ -1561,7 +1561,7 @@ void TextEditor::recompute_visual_lines(size_t line_index)
         auto glyph_spacing = font().glyph_spacing();
         for (size_t i = 0; i < line.length(); ++i) {
             auto code_point = line.code_points()[i];
-            if (isspace(code_point)) {
+            if (is_ascii_space(code_point)) {
                 last_whitespace_index = i;
                 line_width_since_last_whitespace = 0;
             }

Userland/Libraries/LibLine/Editor.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "Editor.h"
+#include <AK/CharacterTypes.h>
 #include <AK/Debug.h>
 #include <AK/GenericLexer.h>
 #include <AK/JsonObject.h>
@@ -19,7 +20,6 @@
 #include <LibCore/EventLoop.h>
 #include <LibCore/File.h>
 #include <LibCore/Notifier.h>
-#include <ctype.h>
 #include <errno.h>
 #include <signal.h>
 #include <stdio.h>
@@ -1338,7 +1338,7 @@ void Editor::refresh_display()
     auto print_character_at = [this](size_t i) {
         StringBuilder builder;
         auto c = m_buffer[i];
-        bool should_print_masked = isascii(c) && iscntrl(c) && c != '\n';
+        bool should_print_masked = is_ascii_control(c) && c != '\n';
         bool should_print_caret = c < 64 && should_print_masked;
         if (should_print_caret)
             builder.appendff("^{:c}", c + 64);
@@ -1722,7 +1722,7 @@ Editor::VTState Editor::actual_rendered_string_length_step(StringMetrics& metric
             current_line.length = 0;
             return state;
         }
-        if (isascii(c) && iscntrl(c) && c != '\n')
+        if (is_ascii_control(c) && c != '\n')
             current_line.masked_chars.append({ index, 1, c < 64 ? 2u : 4u }); // if the character cannot be represented as ^c, represent it as \xbb.
         // FIXME: This will not support anything sophisticated
         ++current_line.length;
@@ -1740,7 +1740,7 @@ Editor::VTState Editor::actual_rendered_string_length_step(StringMetrics& metric
         // FIXME: This does not support non-VT (aside from set-title) escapes
         return state;
     case Bracket:
-        if (isdigit(c)) {
+        if (is_ascii_digit(c)) {
             return BracketArgsSemi;
         }
         return state;
@@ -1748,7 +1748,7 @@ Editor::VTState Editor::actual_rendered_string_length_step(StringMetrics& metric
         if (c == ';') {
             return Bracket;
         }
-        if (!isdigit(c))
+        if (!is_ascii_digit(c))
             state = Free;
         return state;
     case Title:
@@ -1848,7 +1848,7 @@ Vector<size_t, 2> Editor::vt_dsr()
             m_incomplete_data.append(c);
             continue;
         case SawBracket:
-            if (isdigit(c)) {
+            if (is_ascii_digit(c)) {
                 state = InFirstCoordinate;
                 coordinate_buffer.append(c);
                 continue;
@@ -1856,7 +1856,7 @@ Vector<size_t, 2> Editor::vt_dsr()
             m_incomplete_data.append(c);
             continue;
         case InFirstCoordinate:
-            if (isdigit(c)) {
+            if (is_ascii_digit(c)) {
                 coordinate_buffer.append(c);
                 continue;
             }
@@ -1872,7 +1872,7 @@ Vector<size_t, 2> Editor::vt_dsr()
             m_incomplete_data.append(c);
             continue;
         case SawSemicolon:
-            if (isdigit(c)) {
+            if (is_ascii_digit(c)) {
                 state = InSecondCoordinate;
                 coordinate_buffer.append(c);
                 continue;
@@ -1880,7 +1880,7 @@ Vector<size_t, 2> Editor::vt_dsr()
             m_incomplete_data.append(c);
             continue;
         case InSecondCoordinate:
-            if (isdigit(c)) {
+            if (is_ascii_digit(c)) {
                 coordinate_buffer.append(c);
                 continue;
             }

Userland/Libraries/LibLine/InternalFunctions.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <AK/CharacterTypes.h>
 #include <AK/FileStream.h>
 #include <AK/ScopeGuard.h>
 #include <AK/ScopedValueRollback.h>
@@ -11,7 +12,6 @@
 #include <AK/TemporaryChange.h>
 #include <LibCore/File.h>
 #include <LibLine/Editor.h>
-#include <ctype.h>
 #include <stdio.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -84,7 +84,7 @@ void Editor::cursor_left_word()
         for (;;) {
             if (m_cursor == 0)
                 break;
-            if (skipped_at_least_one_character && !isalnum(m_buffer[m_cursor - 1])) // stop *after* a non-alnum, but only if it changes the position
+            if (skipped_at_least_one_character && !is_ascii_alphanumeric(m_buffer[m_cursor - 1])) // stop *after* a non-alnum, but only if it changes the position
                 break;
             skipped_at_least_one_character = true;
             --m_cursor;
@@ -109,7 +109,7 @@ void Editor::cursor_right_word()
         for (;;) {
             if (m_cursor >= m_buffer.size())
                 break;
-            if (!isalnum(m_buffer[++m_cursor]))
+            if (!is_ascii_alphanumeric(m_buffer[++m_cursor]))
                 break;
         }
         m_buffer.take_last();
@@ -178,7 +178,7 @@ void Editor::erase_word_backwards()
     // A word here is space-separated. `foo=bar baz` is two words.
     bool has_seen_nonspace = false;
     while (m_cursor > 0) {
-        if (isspace(m_buffer[m_cursor - 1])) {
+        if (is_ascii_space(m_buffer[m_cursor - 1])) {
             if (has_seen_nonspace)
                 break;
         } else {
@@ -375,27 +375,27 @@ void Editor::transpose_words()
 
     // Move to end of word under (or after) caret.
     size_t cursor = m_cursor;
-    while (cursor < m_buffer.size() && !isalnum(m_buffer[cursor]))
+    while (cursor < m_buffer.size() && !is_ascii_alphanumeric(m_buffer[cursor]))
         ++cursor;
-    while (cursor < m_buffer.size() && isalnum(m_buffer[cursor]))
+    while (cursor < m_buffer.size() && is_ascii_alphanumeric(m_buffer[cursor]))
         ++cursor;
 
     // Move left over second word and the space to its right.
     size_t end = cursor;
     size_t start = cursor;
-    while (start > 0 && !isalnum(m_buffer[start - 1]))
+    while (start > 0 && !is_ascii_alphanumeric(m_buffer[start - 1]))
         --start;
-    while (start > 0 && isalnum(m_buffer[start - 1]))
+    while (start > 0 && is_ascii_alphanumeric(m_buffer[start - 1]))
         --start;
     size_t start_second_word = start;
 
     // Move left over space between the two words.
-    while (start > 0 && !isalnum(m_buffer[start - 1]))
+    while (start > 0 && !is_ascii_alphanumeric(m_buffer[start - 1]))
         --start;
     size_t start_gap = start;
 
     // Move left over first word.
-    while (start > 0 && isalnum(m_buffer[start - 1]))
+    while (start > 0 && is_ascii_alphanumeric(m_buffer[start - 1]))
         --start;
 
     if (start != start_gap) {
@@ -452,7 +452,7 @@ void Editor::erase_alnum_word_backwards()
     // A word here is contiguous alnums. `foo=bar baz` is three words.
     bool has_seen_alnum = false;
     while (m_cursor > 0) {
-        if (!isalnum(m_buffer[m_cursor - 1])) {
+        if (!is_ascii_alphanumeric(m_buffer[m_cursor - 1])) {
             if (has_seen_alnum)
                 break;
         } else {
@@ -467,7 +467,7 @@ void Editor::erase_alnum_word_forwards()
     // A word here is contiguous alnums. `foo=bar baz` is three words.
     bool has_seen_alnum = false;
     while (m_cursor < m_buffer.size()) {
-        if (!isalnum(m_buffer[m_cursor])) {
+        if (!is_ascii_alphanumeric(m_buffer[m_cursor])) {
             if (has_seen_alnum)
                 break;
         } else {
@@ -480,15 +480,15 @@ void Editor::erase_alnum_word_forwards()
 void Editor::case_change_word(Editor::CaseChangeOp change_op)
 {
     // A word here is contiguous alnums. `foo=bar baz` is three words.
-    while (m_cursor < m_buffer.size() && !isalnum(m_buffer[m_cursor]))
+    while (m_cursor < m_buffer.size() && !is_ascii_alphanumeric(m_buffer[m_cursor]))
         ++m_cursor;
     size_t start = m_cursor;
-    while (m_cursor < m_buffer.size() && isalnum(m_buffer[m_cursor])) {
+    while (m_cursor < m_buffer.size() && is_ascii_alphanumeric(m_buffer[m_cursor])) {
         if (change_op == CaseChangeOp::Uppercase || (change_op == CaseChangeOp::Capital && m_cursor == start)) {
-            m_buffer[m_cursor] = toupper(m_buffer[m_cursor]);
+            m_buffer[m_cursor] = to_ascii_uppercase(m_buffer[m_cursor]);
         } else {
             VERIFY(change_op == CaseChangeOp::Lowercase || (change_op == CaseChangeOp::Capital && m_cursor > start));
-            m_buffer[m_cursor] = tolower(m_buffer[m_cursor]);
+            m_buffer[m_cursor] = to_ascii_lowercase(m_buffer[m_cursor]);
         }
         ++m_cursor;
         m_refresh_needed = true;

commit 7da39f2ec45733ce1968c660b996ccbfd63ce353

LibJS: Parse digits with parse_ascii_base36_digit in parseInt
This was accidentally replaced with parse_ascii_hex_digit in
7191c8957c048d947ecc299c776a80e73fd01013 which caused radices above
16 (hex) to fail.

Userland/Libraries/LibJS/Runtime/GlobalObject.cpp
@@ -251,9 +251,9 @@ JS_DEFINE_NATIVE_FUNCTION(GlobalObject::parse_int)
     }
 
     auto parse_digit = [&](u32 code_point, i32 radix) -> Optional<i32> {
-        if (!is_ascii_hex_digit(code_point) || radix <= 0)
+        if (!is_ascii_alphanumeric(code_point) || radix <= 0)
             return {};
-        auto digit = parse_ascii_hex_digit(code_point);
+        auto digit = parse_ascii_base36_digit(code_point);
         if (digit >= (u32)radix)
             return {};
         return digit;