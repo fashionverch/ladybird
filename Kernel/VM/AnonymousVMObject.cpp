fashionverch/ladybird

commit 635a3c0caef281744b1e88a1e8e777d9a38a6b47

Kernel: Uncommit a shared COW page when discovering it was unshared
When we hit a COW fault and discover than no other process is sharing
the physical page, we simply remap it r/w and save ourselves the
trouble. When this happens, we can also give back (uncommit) one of our
shared committed COW pages, since we won't be needing it.

We had this optimization before, but I mistakenly removed it in
16baf33bcfc1aa514b2ca148204faa477dd5791d since I had misunderstood
it to be the reason for a panic.

Kernel/VM/AnonymousVMObject.cpp
@@ -315,6 +315,12 @@ PageFaultResponse AnonymousVMObject::handle_cow_fault(size_t page_index, Virtual
     if (page_slot->ref_count() == 1) {
         dbgln_if(PAGE_FAULT_DEBUG, "    >> It's a COW page but nobody is sharing it anymore. Remap r/w");
         set_should_cow(page_index, false);
+
+        if (m_shared_committed_cow_pages) {
+            m_shared_committed_cow_pages->uncommit_one();
+            if (!m_shared_committed_cow_pages->is_empty())
+                m_shared_committed_cow_pages = nullptr;
+        }
         return PageFaultResponse::Continue;
     }
 
@@ -366,6 +372,12 @@ NonnullRefPtr<PhysicalPage> AnonymousVMObject::SharedCommittedCowPages::take_one
 {
     ScopedSpinLock locker(m_lock);
     return m_committed_pages.take_one();
+}
+
+void AnonymousVMObject::SharedCommittedCowPages::uncommit_one()
+{
+    ScopedSpinLock locker(m_lock);
+    m_committed_pages.uncommit_one();
 }
 
 }

Kernel/VM/AnonymousVMObject.h
@@ -70,8 +70,10 @@ private:
         explicit SharedCommittedCowPages(CommittedPhysicalPageSet&&);
         ~SharedCommittedCowPages();
 
-        [[nodiscard]] NonnullRefPtr<PhysicalPage> take_one();
         [[nodiscard]] bool is_empty() const { return m_committed_pages.is_empty(); }
+
+        [[nodiscard]] NonnullRefPtr<PhysicalPage> take_one();
+        void uncommit_one();
 
     public:
         SpinLock<u8> m_lock;

Kernel/VM/MemoryManager.cpp
@@ -1137,4 +1137,11 @@ NonnullRefPtr<PhysicalPage> CommittedPhysicalPageSet::take_one()
     return MM.allocate_committed_user_physical_page(MemoryManager::ShouldZeroFill::Yes);
 }
 
+void CommittedPhysicalPageSet::uncommit_one()
+{
+    VERIFY(m_page_count > 0);
+    --m_page_count;
+    MM.uncommit_user_physical_pages({}, 1);
+}
+
 }

Kernel/VM/MemoryManager.h
@@ -126,6 +126,7 @@ public:
     size_t page_count() const { return m_page_count; }
 
     [[nodiscard]] NonnullRefPtr<PhysicalPage> take_one();
+    void uncommit_one();
 
     void operator=(CommittedPhysicalPageSet&&) = delete;
 

commit e39e3c007f8e28b63101c7a2699641642969d431

Kernel: Fix logic typo in AnonymousVMObject::handle_cow_fault()
Introduced in 635a3c0caef281744b1e88a1e8e777d9a38a6b47

Kernel/VM/AnonymousVMObject.cpp
@@ -318,7 +318,7 @@ PageFaultResponse AnonymousVMObject::handle_cow_fault(size_t page_index, Virtual
 
         if (m_shared_committed_cow_pages) {
             m_shared_committed_cow_pages->uncommit_one();
-            if (!m_shared_committed_cow_pages->is_empty())
+            if (m_shared_committed_cow_pages->is_empty())
                 m_shared_committed_cow_pages = nullptr;
         }
         return PageFaultResponse::Continue;