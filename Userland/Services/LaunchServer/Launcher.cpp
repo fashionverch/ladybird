fashionverch/ladybird

commit 46d04121a5686ab1fd3105e0f7a73b3b7a806cf4

LaunchServer: Resolve symlinks when querying for handler application
This lets us launch proper handler application for symlinks :^)

Userland/Services/LaunchServer/Launcher.cpp
@@ -12,6 +12,7 @@
 #include <AK/LexicalPath.h>
 #include <AK/StringBuilder.h>
 #include <LibCore/ConfigFile.h>
+#include <LibCore/File.h>
 #include <LibDesktop/AppFile.h>
 #include <errno.h>
 #include <serenity.h>
@@ -252,8 +253,8 @@ void Launcher::for_each_handler(const String& key, HashMap<String, String>& user
 void Launcher::for_each_handler_for_path(const String& path, Function<bool(const Handler&)> f)
 {
     struct stat st;
-    if (stat(path.characters(), &st) < 0) {
-        perror("stat");
+    if (lstat(path.characters(), &st) < 0) {
+        perror("lstat");
         return;
     }
 
@@ -268,6 +269,13 @@ void Launcher::for_each_handler_for_path(const String& path, Function<bool(const
 
     if (!S_ISREG(st.st_mode) && !S_ISLNK(st.st_mode))
         return;
+
+    if (S_ISLNK(st.st_mode)) {
+        auto real_path = Core::File::real_path_for(String::formatted("{}/{}", LexicalPath::dirname(path), Core::File::read_link(path)));
+        return for_each_handler_for_path(real_path, [&](const auto& handler) -> bool {
+            return f(handler);
+        });
+    }
 
     if ((st.st_mode & S_IFMT) == S_IFREG && (st.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)))
         f(get_handler_for_executable(Handler::Type::Application, path));

commit b303f3d9ed13e47d421a660fbb2c12f0ccb9af35

LaunchServer: Make sure launching absolute symlinks works
This was broken by 46d0412.

Userland/Services/LaunchServer/Launcher.cpp
@@ -271,7 +271,9 @@ void Launcher::for_each_handler_for_path(const String& path, Function<bool(const
         return;
 
     if (S_ISLNK(st.st_mode)) {
-        auto real_path = Core::File::real_path_for(String::formatted("{}/{}", LexicalPath::dirname(path), Core::File::read_link(path)));
+        auto link_target = LexicalPath { Core::File::read_link(path) };
+        LexicalPath absolute_link_target = link_target.is_absolute() ? link_target : LexicalPath::join(LexicalPath::dirname(path), link_target.string());
+        auto real_path = Core::File::real_path_for(absolute_link_target.string());
         return for_each_handler_for_path(real_path, [&](const auto& handler) -> bool {
             return f(handler);
         });