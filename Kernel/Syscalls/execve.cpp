fashionverch/ladybird

commit 7d59e7a39dc94cc8fb1ee93e082f0fbdfc1e4b69

Kernel: Unlock ptrace lock before entering a critical section in execve
While it might not be as bad to release a mutex while interrupts are
disabled as it is to acquire one, we don't want to mess with that.

Kernel/Syscalls/execve.cpp
@@ -626,6 +626,7 @@ KResult Process::do_exec(NonnullRefPtr<FileDescription> main_program_description
         ptrace_locker.unlock();
         Thread::current()->send_urgent_signal_to_self(SIGSTOP);
     }
+    ptrace_locker.unlock(); // unlock before disabling interrupts as well
 
     // We enter a critical section here because we don't want to get interrupted between do_exec()
     // and Processor::assume_context() or the next context switch.

commit 5707128d6e4eee0c5605082462836369691f89cb

Kernel: Only unlock Mutex once in execve when PT_TRACE_ME is enabled
Fixes a regression introduced in 7d59e7a. Fixes #9704.

Kernel/Syscalls/execve.cpp
@@ -628,8 +628,11 @@ KResult Process::do_exec(NonnullRefPtr<FileDescription> main_program_description
         // Make sure we release the ptrace lock here or the tracer will block forever.
         ptrace_locker.unlock();
         Thread::current()->send_urgent_signal_to_self(SIGSTOP);
+    } else {
+        // Unlock regardless before disabling interrupts.
+        // Ensure we always unlock after checking ptrace status to avoid TOCTOU ptrace issues
+        ptrace_locker.unlock();
     }
-    ptrace_locker.unlock(); // unlock before disabling interrupts as well
 
     // We enter a critical section here because we don't want to get interrupted between do_exec()
     // and Processor::assume_context() or the next context switch.