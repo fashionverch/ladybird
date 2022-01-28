fashionverch/ladybird

commit 0a9a8d01be7d60511e41dd3222e755b9f6007b91

LibCore+LibIPC+Everywhere: Return Stream::LocalSocket from LocalServer
This change unfortunately cannot be atomically made without a single
commit changing everything.

Most of the important changes are in LibIPC/Connection.cpp,
LibIPC/ServerConnection.cpp and LibCore/LocalServer.cpp.

The notable changes are:
- IPCCompiler now generates the decode and decode_message functions such
  that they take a Core::Stream::LocalSocket instead of the socket fd.
- IPC::Decoder now uses the receive_fd method of LocalSocket instead of
  doing system calls directly on the fd.
- IPC::ConnectionBase and related classes now use the Stream API
  functions.
- IPC::ServerConnection no longer constructs the socket itself; instead,
  a convenience macro, IPC_CLIENT_CONNECTION, is used in place of
  C_OBJECT and will generate a static try_create factory function for
  the ServerConnection subclass. The subclass is now responsible for
  passing the socket constructed in this function to its
  ServerConnection base; the socket is passed as the first argument to
  the constructor (as a NonnullOwnPtr<Core::Stream::LocalServer>) before
  any other arguments.
- The functionality regarding taking over sockets from SystemServer has
  been moved to LibIPC/SystemServerTakeover.cpp. The Core::LocalSocket
  implementation of this functionality hasn't been deleted due to my
  intention of removing this class in the near future and to reduce
  noise on this (already quite noisy) PR.

Userland/DevTools/HackStudio/LanguageClients/ServerConnections.h
@@ -12,19 +12,19 @@
 #include <DevTools/HackStudio/LanguageServers/LanguageServerEndpoint.h>
 #include <LibIPC/ServerConnection.h>
 
-#define LANGUAGE_CLIENT(language_name_, socket_name)                                           \
-    namespace language_name_ {                                                                 \
-    class ServerConnection final : public HackStudio::ServerConnection {                       \
-        C_OBJECT(ServerConnection)                                                             \
-    public:                                                                                    \
-        static const char* language_name() { return #language_name_; }                         \
-                                                                                               \
-    private:                                                                                   \
-        ServerConnection(const String& project_path)                                           \
-            : HackStudio::ServerConnection("/tmp/portal/language/" #socket_name, project_path) \
-        {                                                                                      \
-        }                                                                                      \
-    };                                                                                         \
+#define LANGUAGE_CLIENT(language_name_, socket_name)                                                  \
+    namespace language_name_ {                                                                        \
+    class ServerConnection final : public HackStudio::ServerConnection {                              \
+        IPC_CLIENT_CONNECTION(ServerConnection, "/tmp/portal/language" #socket_name)                  \
+    public:                                                                                           \
+        static const char* language_name() { return #language_name_; }                                \
+                                                                                                      \
+    private:                                                                                          \
+        ServerConnection(NonnullOwnPtr<Core::Stream::LocalSocket> socket, const String& project_path) \
+            : HackStudio::ServerConnection(move(socket), project_path)                                \
+        {                                                                                             \
+        }                                                                                             \
+    };                                                                                                \
     }
 
 namespace LanguageClients {

commit 0fe01c5ce6a29a390b871de13adc1563b2ba267d

HackStudio: Fix path to language-server IPC socket
The path to the language server local socket was accidentally broken
in 0a9a8d0.

Userland/DevTools/HackStudio/LanguageClients/ServerConnections.h
@@ -15,7 +15,7 @@
 #define LANGUAGE_CLIENT(language_name_, socket_name)                                                  \
     namespace language_name_ {                                                                        \
     class ServerConnection final : public HackStudio::ServerConnection {                              \
-        IPC_CLIENT_CONNECTION(ServerConnection, "/tmp/portal/language" #socket_name)                  \
+        IPC_CLIENT_CONNECTION(ServerConnection, "/tmp/portal/language/" #socket_name)                 \
     public:                                                                                           \
         static const char* language_name() { return #language_name_; }                                \
                                                                                                       \