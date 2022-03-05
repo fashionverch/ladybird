fashionverch/ladybird

commit 9f831f90a92c094a3cdbc70465fd3d40d134bc22

LibJS: Add fast_is<PrivateIdentifier>()

Userland/Libraries/LibJS/AST.h
@@ -66,6 +66,7 @@ public:
     virtual bool is_class_expression() const { return false; }
     virtual bool is_expression_statement() const { return false; }
     virtual bool is_identifier() const { return false; }
+    virtual bool is_private_identifier() const { return false; }
     virtual bool is_scope_node() const { return false; }
     virtual bool is_program() const { return false; }
     virtual bool is_class_declaration() const { return false; }
@@ -2088,6 +2089,9 @@ inline bool ASTNode::fast_is<ClassExpression>() const { return is_class_expressi
 
 template<>
 inline bool ASTNode::fast_is<Identifier>() const { return is_identifier(); }
+
+template<>
+inline bool ASTNode::fast_is<PrivateIdentifier>() const { return is_private_identifier(); }
 
 template<>
 inline bool ASTNode::fast_is<ExpressionStatement>() const { return is_expression_statement(); }

commit 65914a6fd7ffad060aa3e8bc90c828490ca3fcd1

LibJS: Actually override is_private_identifier() in PrivateIdentifier
Regression from 9f831f90a92c094a3cdbc70465fd3d40d134bc22.

Userland/Libraries/LibJS/AST.h
@@ -1220,6 +1220,8 @@ public:
     virtual Completion execute(Interpreter&, GlobalObject&) const override;
     virtual void dump(int indent) const override;
 
+    virtual bool is_private_identifier() const override { return true; }
+
 private:
     FlyString m_string;
 };