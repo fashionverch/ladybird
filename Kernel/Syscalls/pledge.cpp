fashionverch/ladybird

commit f85371887c31242f4227c3904db88ed68f0dca83

Kernel: Add a 'no_error' pledge promise
This makes pledge() ignore promises that would otherwise cause it to
fail with EPERM, which is very useful for allowing programs to run under
a "jail" so to speak, without having them termiate early due to a
failing pledge() call.

Kernel/Syscalls/pledge.cpp
@@ -46,16 +46,21 @@ ErrorOr<FlatPtr> Process::sys$pledge(Userspace<const Syscall::SC_pledge_params*>
     if (promises) {
         if (!parse_pledge(promises->view(), new_promises))
             return EINVAL;
-        if (m_protected_values.has_promises && (new_promises & ~m_protected_values.promises))
-            return EPERM;
+
+        if (!(m_protected_values.promises & (1u << (u32)Pledge::no_error))) {
+            if (m_protected_values.has_promises && (new_promises & ~m_protected_values.promises))
+                return EPERM;
+        }
     }
 
     u32 new_execpromises = 0;
     if (execpromises) {
         if (!parse_pledge(execpromises->view(), new_execpromises))
             return EINVAL;
-        if (m_protected_values.has_execpromises && (new_execpromises & ~m_protected_values.execpromises))
-            return EPERM;
+        if (!(m_protected_values.promises & (1u << (u32)Pledge::no_error))) {
+            if (m_protected_values.has_execpromises && (new_execpromises & ~m_protected_values.execpromises))
+                return EPERM;
+        }
     }
 
     // Only apply promises after all validation has occurred, this ensures

Kernel/Process.h
@@ -67,7 +67,8 @@ Time kgettimeofday();
     __ENUMERATE_PLEDGE_PROMISE(setkeymap) \
     __ENUMERATE_PLEDGE_PROMISE(prot_exec) \
     __ENUMERATE_PLEDGE_PROMISE(map_fixed) \
-    __ENUMERATE_PLEDGE_PROMISE(getkeymap)
+    __ENUMERATE_PLEDGE_PROMISE(getkeymap) \
+    __ENUMERATE_PLEDGE_PROMISE(no_error)
 
 enum class Pledge : u32 {
 #define __ENUMERATE_PLEDGE_PROMISE(x) x,

commit 353ed198bfda0f8ff1ee00ae62cfdce2cf76b76b

Kernel: Disallow elevating pledge promises with no_error set
f85371887c31242f4227c3904db88ed68f0dca83 introduced a not-so-subtle bug
where an application with an existing pledge set containing `no_error`
could elevate its pledge set by pledging _anything_, this commit makes
sure that no new promise is accepted.

Kernel/Syscalls/pledge.cpp
@@ -47,9 +47,10 @@ ErrorOr<FlatPtr> Process::sys$pledge(Userspace<const Syscall::SC_pledge_params*>
         if (!parse_pledge(promises->view(), new_promises))
             return EINVAL;
 
-        if (!(m_protected_values.promises & (1u << (u32)Pledge::no_error))) {
-            if (m_protected_values.has_promises && (new_promises & ~m_protected_values.promises))
+        if (m_protected_values.has_promises && (new_promises & ~m_protected_values.promises)) {
+            if (!(m_protected_values.promises & (1u << (u32)Pledge::no_error)))
                 return EPERM;
+            new_promises &= m_protected_values.promises;
         }
     }
 
@@ -57,9 +58,10 @@ ErrorOr<FlatPtr> Process::sys$pledge(Userspace<const Syscall::SC_pledge_params*>
     if (execpromises) {
         if (!parse_pledge(execpromises->view(), new_execpromises))
             return EINVAL;
-        if (!(m_protected_values.promises & (1u << (u32)Pledge::no_error))) {
-            if (m_protected_values.has_execpromises && (new_execpromises & ~m_protected_values.execpromises))
+        if (m_protected_values.has_execpromises && (new_execpromises & ~m_protected_values.execpromises)) {
+            if (!(m_protected_values.promises & (1u << (u32)Pledge::no_error)))
                 return EPERM;
+            new_execpromises &= m_protected_values.execpromises;
         }
     }
 