fashionverch/ladybird

commit 0d1a0c7a293b22490f37c1dcf778d98eff7191da

LibWeb: Preserve paint state when painting stacking contexts indirectly
For layers that require indirect painting (due to opacity, transform,
etc.) we create a nested PaintContext. Until now, that PaintContext
was created fresh without transferring all the state from the parent
PaintContext.

Userland/Libraries/LibWeb/Painting/PaintContext.h
@@ -37,6 +37,16 @@ public:
     bool has_focus() const { return m_focus; }
     void set_has_focus(bool focus) { m_focus = focus; }
 
+    PaintContext clone() const
+    {
+        auto clone = PaintContext(m_painter, m_palette, m_scroll_offset);
+        clone.m_viewport_rect = m_viewport_rect;
+        clone.m_should_show_line_box_borders = m_should_show_line_box_borders;
+        clone.m_focus = m_focus;
+        clone.m_svg_context = m_svg_context;
+        return clone;
+    }
+
 private:
     Gfx::Painter& m_painter;
     Palette m_palette;

Userland/Libraries/LibWeb/Painting/StackingContext.cpp
@@ -262,7 +262,7 @@ void StackingContext::paint(PaintContext& context) const
             return;
         auto bitmap = bitmap_or_error.release_value_but_fixme_should_propagate_errors();
         Gfx::Painter painter(bitmap);
-        PaintContext paint_context(painter, context.palette(), context.scroll_offset());
+        auto paint_context = context.clone();
         paint_internal(paint_context);
 
         auto transform_origin = this->transform_origin();

commit 887ba2eb0dfbcc61d883da229f7f2d75ca10b29d

LibWeb: Use the correct painter for painting stacking contexts
When cloning the PaintContext we should be using the painter backed by
the bitmap created for this stacking context layer.

Fixes: 0d1a0c7a293b ("LibWeb: Preserve paint state when painting...")

Userland/Libraries/LibWeb/Painting/PaintContext.h
@@ -37,9 +37,9 @@ public:
     bool has_focus() const { return m_focus; }
     void set_has_focus(bool focus) { m_focus = focus; }
 
-    PaintContext clone() const
+    PaintContext clone(Gfx::Painter& painter) const
     {
-        auto clone = PaintContext(m_painter, m_palette, m_scroll_offset);
+        auto clone = PaintContext(painter, m_palette, m_scroll_offset);
         clone.m_viewport_rect = m_viewport_rect;
         clone.m_should_show_line_box_borders = m_should_show_line_box_borders;
         clone.m_focus = m_focus;

Userland/Libraries/LibWeb/Painting/StackingContext.cpp
@@ -262,7 +262,7 @@ void StackingContext::paint(PaintContext& context) const
             return;
         auto bitmap = bitmap_or_error.release_value_but_fixme_should_propagate_errors();
         Gfx::Painter painter(bitmap);
-        auto paint_context = context.clone();
+        auto paint_context = context.clone(painter);
         paint_internal(paint_context);
 
         auto transform_origin = this->transform_origin();