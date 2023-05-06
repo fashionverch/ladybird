fashionverch/ladybird

commit a83c8b72cee16ce7c20dfc6e09aa9312170c76e4

LibWeb: Invalidate layout when a DOM node is removed from the tree

Userland/Libraries/LibWeb/DOM/Node.cpp
@@ -604,7 +604,7 @@ void Node::remove(bool suppress_observers)
     // 21. Run the children changed steps for parent.
     parent->children_changed();
 
-    document().invalidate_style();
+    document().invalidate_layout();
 }
 
 // https://dom.spec.whatwg.org/#concept-node-replace

commit e6939bf2c2f978bb9a207a0798c953e2c9716774

LibWeb: Invalidate style *and* layout when removing a DOM node
It's not enough to invalidate only layout, since changes to the DOM tree
may also cause different selectors to apply.

This brings Acid3 back to a score of 100/100. The problem was that a
:last-child selector wasn't rechecked after removing a node that was
previously the last child of its parent.

Regression from a83c8b72cee16ce7c20dfc6e09aa9312170c76e4.

Userland/Libraries/LibWeb/DOM/Node.cpp
@@ -659,6 +659,9 @@ void Node::remove(bool suppress_observers)
     // 21. Run the children changed steps for parent.
     parent->children_changed();
 
+    // Since the tree structure has changed, we need to invalidate both style and layout.
+    // In the future, we should find a way to only invalidate the parts that actually need it.
+    document().invalidate_style();
     document().invalidate_layout();
 }
 