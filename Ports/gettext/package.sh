fashionverch/ladybird

commit 68f79ff60fca1b44021565183a5aacfdce3d6406

Toolchain+Ports: Use `ftpmirror.gnu.org` for faster downloads
This service automatically redirects to a mirror that's geographically
closer, which should make downloading the tarballs faster. The GNU
project recommends this instead of bombarding their top-level downloads
site.

Ports/gettext/package.sh
@@ -2,7 +2,7 @@
 port='gettext'
 version='0.21.1'
 useconfigure='true'
-files="https://ftp.gnu.org/pub/gnu/gettext/gettext-${version}.tar.gz gettext-${version}.tar.gz e8c3650e1d8cee875c4f355642382c1df83058bd5a11ee8555c0cf276d646d45"
+files="https://ftpmirror.gnu.org/pub/gnu/gettext/gettext-${version}.tar.gz gettext-${version}.tar.gz e8c3650e1d8cee875c4f355642382c1df83058bd5a11ee8555c0cf276d646d45"
 auth_type='sha256'
 depends=("libiconv")
 use_fresh_config_sub='true'

Ports/cpio/package.sh
@@ -4,5 +4,5 @@ version='2.13'
 useconfigure='true'
 use_fresh_config_sub='true'
 config_sub_paths=('build-aux/config.sub')
-files="https://ftp.gnu.org/gnu/cpio/cpio-${version}.tar.gz cpio-${version}.tar.gz e87470d9c984317f658567c03bfefb6b0c829ff17dbf6b0de48d71a4c8f3db88"
+files="https://ftpmirror.gnu.org/gnu/cpio/cpio-${version}.tar.gz cpio-${version}.tar.gz e87470d9c984317f658567c03bfefb6b0c829ff17dbf6b0de48d71a4c8f3db88"
 auth_type='sha256'

Ports/findutils/package.sh
@@ -4,5 +4,5 @@ version='4.9.0'
 useconfigure='true'
 use_fresh_config_sub='true'
 config_sub_paths=('build-aux/config.sub')
-files="https://ftp.gnu.org/gnu/findutils/findutils-${version}.tar.xz findutils-${version}.tar.xz a2bfb8c09d436770edc59f50fa483e785b161a3b7b9d547573cb08065fd462fe"
+files="https://ftpmirror.gnu.org/gnu/findutils/findutils-${version}.tar.xz findutils-${version}.tar.xz a2bfb8c09d436770edc59f50fa483e785b161a3b7b9d547573cb08065fd462fe"
 auth_type='sha256'

Ports/gsl/package.sh
@@ -3,8 +3,8 @@
 port=gsl
 version=2.7.1
 useconfigure=true
-files="https://ftp.gnu.org/gnu/gsl/gsl-${version}.tar.gz gsl-${version}.tar.gz
-https://ftp.gnu.org/gnu/gsl/gsl-${version}.tar.gz.sig gsl-${version}.tar.gz.sig
+files="https://ftpmirror.gnu.org/gnu/gsl/gsl-${version}.tar.gz gsl-${version}.tar.gz
+https://ftpmirror.gnu.org/gnu/gsl/gsl-${version}.tar.gz.sig gsl-${version}.tar.gz.sig
 https://ftpmirror.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
 auth_type=sig
 auth_opts=("--keyring" "./gnu-keyring.gpg" "gsl-${version}.tar.gz.sig")

Ports/gzip/package.sh
@@ -4,5 +4,5 @@ version='1.12'
 useconfigure='true'
 use_fresh_config_sub='true'
 config_sub_paths=('build-aux/config.sub')
-files="https://ftp.gnu.org/gnu/gzip/gzip-${version}.tar.gz gzip-${version}.tar.gz 5b4fb14d38314e09f2fc8a1c510e7cd540a3ea0e3eb9b0420046b82c3bf41085"
+files="https://ftpmirror.gnu.org/gnu/gzip/gzip-${version}.tar.gz gzip-${version}.tar.gz 5b4fb14d38314e09f2fc8a1c510e7cd540a3ea0e3eb9b0420046b82c3bf41085"
 auth_type='sha256'

Ports/patch/package.sh
@@ -4,5 +4,5 @@ version=2.7.6
 useconfigure=true
 use_fresh_config_sub=true
 config_sub_paths=("build-aux/config.sub")
-files="https://ftp.gnu.org/gnu/patch/patch-${version}.tar.gz patch-${version}.tar.gz 8cf86e00ad3aaa6d26aca30640e86b0e3e1f395ed99f189b06d4c9f74bc58a4e"
+files="https://ftpmirror.gnu.org/gnu/patch/patch-${version}.tar.gz patch-${version}.tar.gz 8cf86e00ad3aaa6d26aca30640e86b0e3e1f395ed99f189b06d4c9f74bc58a4e"
 auth_type=sha256

Ports/tar/package.sh
@@ -4,7 +4,7 @@ version='1.34'
 useconfigure='true'
 use_fresh_config_sub='true'
 config_sub_paths=('build-aux/config.sub')
-files="https://ftp.gnu.org/gnu/tar/tar-${version}.tar.gz tar-${version}.tar.gz 03d908cf5768cfe6b7ad588c921c6ed21acabfb2b79b788d1330453507647aed"
+files="https://ftpmirror.gnu.org/gnu/tar/tar-${version}.tar.gz tar-${version}.tar.gz 03d908cf5768cfe6b7ad588c921c6ed21acabfb2b79b788d1330453507647aed"
 auth_type='sha256'
 configopts=(
     "--without-selinux"

Ports/which/package.sh
@@ -2,5 +2,5 @@
 port='which'
 version='2.21'
 useconfigure='true'
-files="https://ftp.gnu.org/gnu/which/which-${version}.tar.gz which-${version}.tar.gz f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad"
+files="https://ftpmirror.gnu.org/gnu/which/which-${version}.tar.gz which-${version}.tar.gz f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad"
 auth_type='sha256'

commit b67ab95f2376d3775f51905ae37bad3785de007d

Ports: Point to the correct subdirectory for `gettext`
`ftpmirror.gnu.org` does not seem to have the `/pub/gnu` folder
structure, all files are in `/gnu` instead.

This regressed in 68f79ff60fca1b44021565183a5aacfdce3d6406.

Ports/gettext/package.sh
@@ -2,7 +2,7 @@
 port='gettext'
 version='0.21.1'
 useconfigure='true'
-files="https://ftpmirror.gnu.org/pub/gnu/gettext/gettext-${version}.tar.gz gettext-${version}.tar.gz e8c3650e1d8cee875c4f355642382c1df83058bd5a11ee8555c0cf276d646d45"
+files="https://ftpmirror.gnu.org/gettext/gettext-${version}.tar.gz gettext-${version}.tar.gz e8c3650e1d8cee875c4f355642382c1df83058bd5a11ee8555c0cf276d646d45"
 auth_type='sha256'
 depends=("libiconv")
 use_fresh_config_sub='true'