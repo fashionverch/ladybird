fashionverch/ladybird

commit 1f065fc70f84356002a120510c657bcb60caf7d0

LibWeb: Disambiguate GridTrackPlacement API
- Ambiguous `raw_value()` method is replaced with `line_number()` and
  `span()`.
- `line_name()` that before returned either line name or area name is
  replaced with `line_name()` and `area_name()`.
- `Position` type is replaced with `Line` and `Area` type so we don't
   have to guess while doing layout.

Affected test expectations:
- `template-lines-and-areas` - improvement over what we had before.
- `named-tracks` - rebaseline a giant test. will have to split it into
  smaller tests in the future.

Userland/Libraries/LibWeb/CSS/GridTrackPlacement.cpp
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2023, Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
  * Copyright (c) 2022, Martin Falisse <mfalisse@outlook.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
@@ -9,48 +10,24 @@
 
 namespace Web::CSS {
 
-GridTrackPlacement::GridTrackPlacement(int span_count_or_position, bool has_span)
-    : m_type(has_span ? Type::Span : Type::Position)
-    , m_span_count_or_position(span_count_or_position)
-{
-}
-
-GridTrackPlacement::GridTrackPlacement(String line_name, int span_count_or_position, bool has_span)
-    : m_type(has_span ? Type::Span : Type::Position)
-    , m_span_count_or_position(span_count_or_position)
-    , m_line_name(line_name)
-{
-}
-
-GridTrackPlacement::GridTrackPlacement(String line_name, bool has_span)
-    : m_type(has_span ? Type::Span : Type::Position)
-    , m_line_name(line_name)
-{
-}
-
-GridTrackPlacement::GridTrackPlacement()
-    : m_type(Type::Auto)
-{
-}
-
 String GridTrackPlacement::to_string() const
 {
     StringBuilder builder;
-    if (is_auto()) {
-        builder.append("auto"sv);
-        return MUST(builder.to_string());
-    }
-    if (is_span()) {
-        builder.append("span"sv);
-        builder.append(" "sv);
-    }
-    if (m_span_count_or_position != 0) {
-        builder.append(MUST(String::number(m_span_count_or_position)));
-        builder.append(" "sv);
-    }
-    if (has_line_name()) {
-        builder.append(m_line_name);
-    }
+    m_value.visit(
+        [&](Auto const&) {
+            builder.append("auto"sv);
+        },
+        [&](Area const& area) {
+            builder.append(area.name);
+        },
+        [&](Line const& line) {
+            builder.appendff("{}", line.value);
+            if (line.name.has_value())
+                builder.appendff(" {}", line.name.value());
+        },
+        [&](Span const& span) {
+            builder.appendff("span {}", span.value);
+        });
     return MUST(builder.to_string());
 }
 

Userland/Libraries/LibWeb/CSS/GridTrackPlacement.h
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2023, Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
  * Copyright (c) 2022, Martin Falisse <mfalisse@outlook.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
@@ -12,40 +13,79 @@ namespace Web::CSS {
 
 class GridTrackPlacement {
 public:
-    enum class Type {
-        Span,
-        Position,
-        Auto
-    };
-
-    GridTrackPlacement(String line_name, int span_count_or_position, bool has_span = false);
-    GridTrackPlacement(int span_count_or_position, bool has_span = false);
-    GridTrackPlacement(String line_name, bool has_span = false);
-    GridTrackPlacement();
-
-    static GridTrackPlacement make_auto() { return GridTrackPlacement(); }
-
-    bool is_span() const { return m_type == Type::Span; }
-    bool is_position() const { return m_type == Type::Position; }
-    bool is_auto() const { return m_type == Type::Auto; }
-    bool is_auto_positioned() const { return m_type == Type::Auto || (m_type == Type::Span && !has_line_name()); }
-
-    bool has_line_name() const { return !m_line_name.is_empty(); }
-
-    int raw_value() const { return m_span_count_or_position; }
-    Type type() const { return m_type; }
-    String line_name() const { return m_line_name; }
-
-    String to_string() const;
-    bool operator==(GridTrackPlacement const& other) const
+    static GridTrackPlacement make_auto()
     {
-        return m_type == other.type() && m_span_count_or_position == other.raw_value();
+        return GridTrackPlacement();
     }
 
+    static GridTrackPlacement make_area(String name)
+    {
+        return GridTrackPlacement(Area { .name = name });
+    }
+
+    static GridTrackPlacement make_line(int value, Optional<String> name)
+    {
+        return GridTrackPlacement(Line { .value = value, .name = name });
+    }
+
+    static GridTrackPlacement make_span(int value)
+    {
+        return GridTrackPlacement(Span { .value = value });
+    }
+
+    bool is_auto() const { return m_value.has<Auto>(); }
+    bool is_area() const { return m_value.has<Area>(); }
+    bool is_line() const { return m_value.has<Line>(); }
+    bool is_span() const { return m_value.has<Span>(); }
+
+    bool is_auto_positioned() const { return is_auto() || is_span(); }
+    bool is_positioned() const { return !is_auto_positioned(); }
+
+    bool has_line_name() const
+    {
+        return is_line() && m_value.get<Line>().name.has_value();
+    }
+
+    String area_name() const { return m_value.get<Area>().name; }
+    String line_name() const { return m_value.get<Line>().name.value(); }
+    int line_number() const { return m_value.get<Line>().value; }
+    int span() const { return m_value.get<Span>().value; }
+
+    String to_string() const;
+
+    bool operator==(GridTrackPlacement const& other) const = default;
+
 private:
-    Type m_type;
-    int m_span_count_or_position { 0 };
-    String m_line_name;
+    struct Auto {
+        bool operator==(Auto const&) const = default;
+    };
+
+    struct Area {
+        String name;
+        bool operator==(Area const& other) const = default;
+    };
+
+    struct Line {
+        int value;
+        Optional<String> name;
+        bool operator==(Line const& other) const = default;
+    };
+
+    struct Span {
+        int value;
+        bool operator==(Span const& other) const = default;
+    };
+
+    GridTrackPlacement()
+        : m_value(Auto {}) {};
+    GridTrackPlacement(Area value)
+        : m_value(value) {};
+    GridTrackPlacement(Line value)
+        : m_value(value) {};
+    GridTrackPlacement(Span value)
+        : m_value(value) {};
+
+    Variant<Auto, Area, Line, Span> m_value;
 };
 
 }

Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
@@ -5350,7 +5350,7 @@ RefPtr<StyleValue> Parser::parse_grid_track_placement(Vector<ComponentValue> con
             return true;
         return false;
     };
-    auto is_line_name = [](Token token) -> bool {
+    auto is_identifier = [](Token token) -> bool {
         // The <custom-ident> additionally excludes the keywords span and auto.
         if (token.is(Token::Type::Ident) && !token.ident().equals_ignoring_ascii_case("span"sv) && !token.ident().equals_ignoring_ascii_case("auto"sv))
             return true;
@@ -5363,22 +5363,22 @@ RefPtr<StyleValue> Parser::parse_grid_track_placement(Vector<ComponentValue> con
 
     if (!tokens.has_next_token()) {
         if (is_auto(current_token))
-            return GridTrackPlacementStyleValue::create(CSS::GridTrackPlacement());
+            return GridTrackPlacementStyleValue::create(GridTrackPlacement::make_auto());
         if (is_span(current_token))
-            return GridTrackPlacementStyleValue::create(CSS::GridTrackPlacement(1, true));
+            return GridTrackPlacementStyleValue::create(GridTrackPlacement::make_span(1));
         if (is_valid_integer(current_token))
-            return GridTrackPlacementStyleValue::create(CSS::GridTrackPlacement(static_cast<int>(current_token.number_value())));
-        if (is_line_name(current_token)) {
+            return GridTrackPlacementStyleValue::create(GridTrackPlacement::make_line(static_cast<int>(current_token.number_value()), {}));
+        if (is_identifier(current_token)) {
             auto maybe_string = String::from_utf8(current_token.ident());
             if (!maybe_string.is_error())
-                return GridTrackPlacementStyleValue::create(CSS::GridTrackPlacement(maybe_string.value()));
+                return GridTrackPlacementStyleValue::create(GridTrackPlacement::make_area(maybe_string.value()));
         }
         return nullptr;
     }
 
     auto span_value = false;
     auto span_or_position_value = 0;
-    String line_name_value;
+    String identifier_value;
     while (true) {
         if (is_auto(current_token))
             return nullptr;
@@ -5394,12 +5394,12 @@ RefPtr<StyleValue> Parser::parse_grid_track_placement(Vector<ComponentValue> con
             else
                 return nullptr;
         }
-        if (is_line_name(current_token)) {
-            if (line_name_value.is_empty()) {
+        if (is_identifier(current_token)) {
+            if (identifier_value.is_empty()) {
                 auto maybe_string = String::from_utf8(current_token.ident());
                 if (maybe_string.is_error())
                     return nullptr;
-                line_name_value = maybe_string.release_value();
+                identifier_value = maybe_string.release_value();
             } else {
                 return nullptr;
             }
@@ -5419,9 +5419,9 @@ RefPtr<StyleValue> Parser::parse_grid_track_placement(Vector<ComponentValue> con
     if (span_or_position_value == 0)
         span_or_position_value = 1;
 
-    if (!line_name_value.is_empty())
-        return GridTrackPlacementStyleValue::create(CSS::GridTrackPlacement(line_name_value, span_or_position_value, span_value));
-    return GridTrackPlacementStyleValue::create(CSS::GridTrackPlacement(span_or_position_value, span_value));
+    if (!identifier_value.is_empty())
+        return GridTrackPlacementStyleValue::create(GridTrackPlacement::make_line(span_or_position_value, identifier_value));
+    return GridTrackPlacementStyleValue::create(GridTrackPlacement::make_span(span_or_position_value));
 }
 
 RefPtr<StyleValue> Parser::parse_grid_track_placement_shorthand_value(Vector<ComponentValue> const& component_values)

Userland/Libraries/LibWeb/Layout/GridFormattingContext.h
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2023, Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
  * Copyright (c) 2022-2023, Martin Falisse <mfalisse@outlook.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause

commit af409889866941eb889deee92e3a011cacdc6447

LibWeb: Define if identifier represent area or line during layout [GFC]
This fixes regression introduced in
1f065fc70f84356002a120510c657bcb60caf7d0 by assuming that it is
possible to determine whether identifier stands for line or area
during parsing.

Userland/Libraries/LibWeb/CSS/GridTrackPlacement.cpp
@@ -17,13 +17,15 @@ String GridTrackPlacement::to_string() const
         [&](Auto const&) {
             builder.append("auto"sv);
         },
-        [&](Area const& area) {
-            builder.append(area.name);
-        },
-        [&](Line const& line) {
-            builder.appendff("{}", line.value);
-            if (line.name.has_value())
-                builder.appendff(" {}", line.name.value());
+        [&](AreaOrLine const& area_or_line) {
+            if (area_or_line.line_number.has_value() && area_or_line.name.has_value()) {
+                builder.appendff("{} {}", *area_or_line.line_number, *area_or_line.name);
+            } else if (area_or_line.line_number.has_value()) {
+                builder.appendff("{}", *area_or_line.line_number);
+            }
+            if (area_or_line.name.has_value()) {
+                builder.appendff("{}", *area_or_line.name);
+            }
         },
         [&](Span const& span) {
             builder.appendff("span {}", span.value);

Userland/Libraries/LibWeb/CSS/GridTrackPlacement.h
@@ -18,14 +18,9 @@ public:
         return GridTrackPlacement();
     }
 
-    static GridTrackPlacement make_area(String name)
+    static GridTrackPlacement make_line(Optional<int> line_number, Optional<String> name)
     {
-        return GridTrackPlacement(Area { .name = name });
-    }
-
-    static GridTrackPlacement make_line(int value, Optional<String> name)
-    {
-        return GridTrackPlacement(Line { .value = value, .name = name });
+        return GridTrackPlacement(AreaOrLine { .line_number = line_number, .name = name });
     }
 
     static GridTrackPlacement make_span(int value)
@@ -34,21 +29,25 @@ public:
     }
 
     bool is_auto() const { return m_value.has<Auto>(); }
-    bool is_area() const { return m_value.has<Area>(); }
-    bool is_line() const { return m_value.has<Line>(); }
     bool is_span() const { return m_value.has<Span>(); }
+    bool is_area_or_line() const { return m_value.has<AreaOrLine>(); }
 
     bool is_auto_positioned() const { return is_auto() || is_span(); }
     bool is_positioned() const { return !is_auto_positioned(); }
 
-    bool has_line_name() const
+    bool has_identifier() const
     {
-        return is_line() && m_value.get<Line>().name.has_value();
+        return is_area_or_line() && m_value.get<AreaOrLine>().name.has_value();
     }
 
-    String area_name() const { return m_value.get<Area>().name; }
-    String line_name() const { return m_value.get<Line>().name.value(); }
-    int line_number() const { return m_value.get<Line>().value; }
+    bool has_line_number() const
+    {
+        return is_area_or_line() && m_value.get<AreaOrLine>().line_number.has_value();
+    }
+
+    String identifier() const { return *m_value.get<AreaOrLine>().name; }
+
+    int line_number() const { return *m_value.get<AreaOrLine>().line_number; }
     int span() const { return m_value.get<Span>().value; }
 
     String to_string() const;
@@ -60,15 +59,10 @@ private:
         bool operator==(Auto const&) const = default;
     };
 
-    struct Area {
-        String name;
-        bool operator==(Area const& other) const = default;
-    };
-
-    struct Line {
-        int value;
+    struct AreaOrLine {
+        Optional<int> line_number;
         Optional<String> name;
-        bool operator==(Line const& other) const = default;
+        bool operator==(AreaOrLine const& other) const = default;
     };
 
     struct Span {
@@ -78,14 +72,12 @@ private:
 
     GridTrackPlacement()
         : m_value(Auto {}) {};
-    GridTrackPlacement(Area value)
-        : m_value(value) {};
-    GridTrackPlacement(Line value)
+    GridTrackPlacement(AreaOrLine value)
         : m_value(value) {};
     GridTrackPlacement(Span value)
         : m_value(value) {};
 
-    Variant<Auto, Area, Line, Span> m_value;
+    Variant<Auto, AreaOrLine, Span> m_value;
 };
 
 }

Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
@@ -5371,7 +5371,7 @@ RefPtr<StyleValue> Parser::parse_grid_track_placement(Vector<ComponentValue> con
         if (is_identifier(current_token)) {
             auto maybe_string = String::from_utf8(current_token.ident());
             if (!maybe_string.is_error())
-                return GridTrackPlacementStyleValue::create(GridTrackPlacement::make_area(maybe_string.value()));
+                return GridTrackPlacementStyleValue::create(GridTrackPlacement::make_line({}, maybe_string.value()));
         }
         return nullptr;
     }

Userland/Libraries/LibWeb/Layout/GridFormattingContext.cpp
@@ -136,13 +136,13 @@ void GridFormattingContext::place_item_with_row_and_column_position(Box const& c
 
     int row_start = 0, row_end = 0, column_start = 0, column_end = 0;
 
-    if (grid_row_start.is_line())
+    if (grid_row_start.has_line_number())
         row_start = child_box.computed_values().grid_row_start().line_number() - 1;
-    if (grid_row_end.is_line())
+    if (grid_row_end.has_line_number())
         row_end = child_box.computed_values().grid_row_end().line_number() - 1;
-    if (grid_column_start.is_line())
+    if (grid_column_start.has_line_number())
         column_start = child_box.computed_values().grid_column_start().line_number() - 1;
-    if (grid_column_end.is_line())
+    if (grid_column_end.has_line_number())
         column_end = child_box.computed_values().grid_column_end().line_number() - 1;
 
     // https://www.w3.org/TR/css-grid-2/#line-placement
@@ -174,15 +174,15 @@ void GridFormattingContext::place_item_with_row_and_column_position(Box const& c
     // grid-column-start line.
     size_t row_span = 1;
     size_t column_span = 1;
-    if (grid_row_start.is_line() && grid_row_end.is_span())
+    if (grid_row_start.has_line_number() && grid_row_end.is_span())
         row_span = grid_row_end.span();
-    if (grid_column_start.is_line() && grid_column_end.is_span())
+    if (grid_column_start.has_line_number() && grid_column_end.is_span())
         column_span = grid_column_end.span();
-    if (grid_row_end.is_line() && child_box.computed_values().grid_row_start().is_span()) {
+    if (grid_row_end.has_line_number() && child_box.computed_values().grid_row_start().is_span()) {
         row_span = grid_row_start.span();
         row_start = row_end - row_span;
     }
-    if (grid_column_end.is_line() && grid_column_start.is_span()) {
+    if (grid_column_end.has_line_number() && grid_column_start.is_span()) {
         column_span = grid_column_start.span();
         column_start = column_end - column_span;
     }
@@ -200,54 +200,39 @@ void GridFormattingContext::place_item_with_row_and_column_position(Box const& c
     // https://www.w3.org/TR/css-grid-2/#common-uses-named-lines
     // 8.1.3. Named Lines and Spans
     // Instead of counting lines by number, lines can be referenced by their line name:
-    if (grid_column_end.is_area()) {
-        if (auto maybe_grid_area = m_grid_areas.get(grid_column_end.area_name()); maybe_grid_area.has_value())
+    if (grid_column_end.has_identifier()) {
+        if (auto maybe_grid_area = m_grid_areas.get(grid_column_end.identifier()); maybe_grid_area.has_value())
             column_end = maybe_grid_area->column_end;
-        else
-            column_end = 1;
-        column_start = column_end - 1;
-    } else if (grid_column_end.has_line_name()) {
-        if (auto line_name_index = get_line_index_by_line_name(grid_column_end.line_name(), grid_container().computed_values().grid_template_columns()); line_name_index > -1)
+        else if (auto line_name_index = get_line_index_by_line_name(grid_column_end.identifier(), grid_container().computed_values().grid_template_columns()); line_name_index > -1)
             column_end = line_name_index;
         else
             column_end = 1;
         column_start = column_end - 1;
     }
 
-    if (grid_column_start.is_area()) {
-        if (auto maybe_grid_area = m_grid_areas.get(grid_column_start.area_name()); maybe_grid_area.has_value())
+    if (grid_column_start.has_identifier()) {
+        if (auto maybe_grid_area = m_grid_areas.get(grid_column_start.identifier()); maybe_grid_area.has_value())
             column_start = maybe_grid_area->column_start;
-        else
-            column_start = 0;
-    }
-    if (grid_column_start.has_line_name()) {
-        if (auto line_name_index = get_line_index_by_line_name(grid_column_start.line_name(), grid_container().computed_values().grid_template_columns()); line_name_index > -1)
+        else if (auto line_name_index = get_line_index_by_line_name(grid_column_start.identifier(), grid_container().computed_values().grid_template_columns()); line_name_index > -1)
             column_start = line_name_index;
         else
             column_start = 0;
     }
 
-    if (grid_row_end.is_area()) {
-        if (auto maybe_grid_area = m_grid_areas.get(grid_row_end.area_name()); maybe_grid_area.has_value())
+    if (grid_row_end.has_identifier()) {
+        if (auto maybe_grid_area = m_grid_areas.get(grid_row_end.identifier()); maybe_grid_area.has_value())
             row_end = maybe_grid_area->row_end;
-        else
-            row_end = 1;
-        row_start = row_end - 1;
-    } else if (grid_row_end.has_line_name()) {
-        if (auto line_name_index = get_line_index_by_line_name(grid_row_end.line_name(), grid_container().computed_values().grid_template_rows()); line_name_index > -1)
+        else if (auto line_name_index = get_line_index_by_line_name(grid_row_end.identifier(), grid_container().computed_values().grid_template_rows()); line_name_index > -1)
             row_end = line_name_index;
         else
             row_end = 1;
         row_start = row_end - 1;
     }
 
-    if (grid_row_start.is_area()) {
-        if (auto maybe_grid_area = m_grid_areas.get(grid_row_start.area_name()); maybe_grid_area.has_value())
+    if (grid_row_start.has_identifier()) {
+        if (auto maybe_grid_area = m_grid_areas.get(grid_row_start.identifier()); maybe_grid_area.has_value())
             row_start = maybe_grid_area->row_start;
-        else
-            row_start = 0;
-    } else if (grid_row_start.has_line_name()) {
-        if (auto line_name_index = get_line_index_by_line_name(grid_row_start.line_name(), grid_container().computed_values().grid_template_rows()); line_name_index > -1)
+        else if (auto line_name_index = get_line_index_by_line_name(grid_row_start.identifier(), grid_container().computed_values().grid_template_rows()); line_name_index > -1)
             row_start = line_name_index;
         else
             row_start = 0;
@@ -301,9 +286,9 @@ void GridFormattingContext::place_item_with_row_position(Box const& child_box)
 
     int row_start = 0, row_end = 0;
 
-    if (grid_row_start.is_line())
+    if (grid_row_start.has_line_number())
         row_start = grid_row_start.line_number() - 1;
-    if (grid_row_end.is_line())
+    if (grid_row_end.has_line_number())
         row_end = grid_row_end.line_number() - 1;
 
     // https://www.w3.org/TR/css-grid-2/#line-placement
@@ -332,9 +317,9 @@ void GridFormattingContext::place_item_with_row_position(Box const& child_box)
     // grid-column-end: span 2 indicates the second grid line in the endward direction from the
     // grid-column-start line.
     size_t row_span = 1;
-    if (grid_row_start.is_line() && grid_row_end.is_span())
+    if (grid_row_start.has_line_number() && grid_row_end.is_span())
         row_span = grid_row_end.span();
-    if (grid_row_end.is_line() && grid_row_start.is_span()) {
+    if (grid_row_end.has_line_number() && grid_row_start.is_span()) {
         row_span = grid_row_start.span();
         row_start = row_end - row_span;
         // FIXME: Remove me once have implemented spans overflowing into negative indexes, e.g., grid-row: span 2 / 1
@@ -355,27 +340,20 @@ void GridFormattingContext::place_item_with_row_position(Box const& child_box)
     // https://www.w3.org/TR/css-grid-2/#common-uses-named-lines
     // 8.1.3. Named Lines and Spans
     // Instead of counting lines by number, lines can be referenced by their line name:
-    if (grid_row_end.is_area()) {
-        if (auto maybe_grid_area = m_grid_areas.get(grid_row_end.area_name()); maybe_grid_area.has_value())
+    if (grid_row_end.has_identifier()) {
+        if (auto maybe_grid_area = m_grid_areas.get(grid_row_end.identifier()); maybe_grid_area.has_value())
             row_end = maybe_grid_area->row_end;
-        else
-            row_end = 1;
-        row_start = row_end - 1;
-    } else if (grid_row_end.has_line_name()) {
-        if (auto line_name_index = get_line_index_by_line_name(grid_row_end.line_name(), grid_container().computed_values().grid_template_rows()); line_name_index > -1)
+        else if (auto line_name_index = get_line_index_by_line_name(grid_row_end.identifier(), grid_container().computed_values().grid_template_rows()); line_name_index > -1)
             row_end = line_name_index - 1;
         else
             row_end = 1;
         row_start = row_end - 1;
     }
 
-    if (grid_row_start.is_area()) {
-        if (auto maybe_grid_area = m_grid_areas.get(grid_row_start.area_name()); maybe_grid_area.has_value())
+    if (grid_row_start.has_identifier()) {
+        if (auto maybe_grid_area = m_grid_areas.get(grid_row_start.identifier()); maybe_grid_area.has_value())
             row_start = maybe_grid_area->row_start;
-        else
-            row_start = 0;
-    } else if (grid_row_start.has_line_name()) {
-        if (auto line_name_index = get_line_index_by_line_name(grid_row_start.line_name(), grid_container().computed_values().grid_template_rows()); line_name_index > -1)
+        else if (auto line_name_index = get_line_index_by_line_name(grid_row_start.identifier(), grid_container().computed_values().grid_template_rows()); line_name_index > -1)
             row_start = line_name_index;
         else
             row_start = 0;
@@ -436,15 +414,15 @@ void GridFormattingContext::place_item_with_column_position(Box const& child_box
     auto const& grid_column_end = child_box.computed_values().grid_column_end();
 
     int column_start = 0;
-    if (grid_column_start.is_line() && grid_column_start.line_number() > 0) {
+    if (grid_column_start.has_line_number() && grid_column_start.line_number() > 0) {
         column_start = grid_column_start.line_number() - 1;
-    } else if (grid_column_start.is_line()) {
+    } else if (grid_column_start.has_line_number()) {
         // NOTE: Negative indexes count from the end side of the explicit grid
         column_start = m_explicit_columns_line_count + grid_column_start.line_number();
     }
 
     int column_end = 0;
-    if (grid_column_end.is_line())
+    if (grid_column_end.has_line_number())
         column_end = grid_column_end.line_number() - 1;
 
     // https://www.w3.org/TR/css-grid-2/#line-placement
@@ -474,9 +452,9 @@ void GridFormattingContext::place_item_with_column_position(Box const& child_box
     // grid-column-start line.
     size_t column_span = 1;
     size_t row_span = grid_row_start.is_span() ? grid_row_start.span() : 1;
-    if (grid_column_start.is_line() && grid_column_end.is_span())
+    if (grid_column_start.has_line_number() && grid_column_end.is_span())
         column_span = grid_column_end.span();
-    if (grid_column_end.is_line() && grid_column_start.is_span()) {
+    if (grid_column_end.has_line_number() && grid_column_start.is_span()) {
         column_span = grid_column_start.span();
         column_start = column_end - column_span;
         // FIXME: Remove me once have implemented spans overflowing into negative indexes, e.g., grid-column: span 2 / 1
@@ -500,31 +478,24 @@ void GridFormattingContext::place_item_with_column_position(Box const& child_box
     // https://www.w3.org/TR/css-grid-2/#common-uses-named-lines
     // 8.1.3. Named Lines and Spans
     // Instead of counting lines by number, lines can be referenced by their line name:
-    if (grid_column_end.is_area()) {
-        if (auto maybe_grid_area = m_grid_areas.get(grid_column_end.area_name()); maybe_grid_area.has_value())
+    if (grid_column_end.has_identifier()) {
+        if (auto maybe_grid_area = m_grid_areas.get(grid_column_end.identifier()); maybe_grid_area.has_value())
             column_end = maybe_grid_area->column_end;
-        else
-            column_end = 1;
-        column_start = column_end - 1;
-    } else if (grid_column_end.has_line_name()) {
-        if (auto line_name_index = get_line_index_by_line_name(grid_column_end.line_name(), grid_container().computed_values().grid_template_columns()); line_name_index > -1)
+        else if (auto line_name_index = get_line_index_by_line_name(grid_column_end.identifier(), grid_container().computed_values().grid_template_columns()); line_name_index > -1)
             column_end = line_name_index;
         else
             column_end = 1;
         column_start = column_end - 1;
     }
 
-    if (grid_column_start.is_area()) {
-        if (auto maybe_grid_area = m_grid_areas.get(grid_column_start.area_name()); maybe_grid_area.has_value())
+    if (grid_column_start.has_identifier()) {
+        if (auto maybe_grid_area = m_grid_areas.get(grid_column_start.identifier()); maybe_grid_area.has_value())
             column_start = maybe_grid_area->column_start;
-        else
-            column_start = 0;
-    }
-    if (grid_column_start.has_line_name()) {
-        if (auto line_name_index = get_line_index_by_line_name(grid_column_start.line_name(), grid_container().computed_values().grid_template_columns()); line_name_index > -1)
+        else if (auto line_name_index = get_line_index_by_line_name(grid_column_start.identifier(), grid_container().computed_values().grid_template_columns()); line_name_index > -1) {
             column_start = line_name_index;
-        else
+        } else {
             column_start = 0;
+        }
     }
 
     // If there are multiple lines of the same name, they effectively establish a named set of grid

Tests/LibWeb/Layout/input/grid/placement-using-named-tracks-1.html
@@ -1,6 +1,4 @@
 <style>
-    /* FIXME: This test is not rendered correctly */
-
     * {
         border: 1px solid black;
     }

Tests/LibWeb/Layout/input/grid/placement-using-named-tracks-2.html
@@ -1,6 +1,4 @@
 <style>
-    /* FIXME: This test is not rendered correctly */
-
     * {
         border: 1px solid black;
     }

Tests/LibWeb/Layout/input/grid/placement-using-named-tracks-3.html
@@ -1,6 +1,4 @@
 <style>
-    /* FIXME: This test is not rendered correctly */
-
     * {
         border: 1px solid black;
     }