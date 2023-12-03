fashionverch/ladybird

commit 42aacef1d5c00b7c00cbc86c5fc259b0fcb0cd70

LibGfx+LibWeb: Calculate and save glyph positions during layout
Previously, we determined the positions of glyphs for each text run at
the time of painting, which constituted a significant portion of the
painting process according to profiles. However, since we already go
through each glyph to figure out the width of each fragment during
layout, we can simultaneously gather data about the position of each
glyph in the layout phase and utilize this information in the painting
phase.

I had to update expectations for a couple of reference tests. These
updates are due to the fact that we now measure glyph positions during
layout using a 1x font, and then linearly scale each glyph's position
to device pixels during painting. This approach should be acceptable,
considering we measure a fragment's width and height with an unscaled
font during layout.

Userland/Libraries/LibWeb/Painting/PaintableBox.cpp
@@ -645,18 +645,24 @@ static void paint_text_fragment(PaintContext& context, Layout::TextNode const& t
         auto text = text_node.text_for_rendering();
 
         DevicePixelPoint baseline_start { fragment_absolute_device_rect.x(), fragment_absolute_device_rect.y() + context.rounded_device_pixels(fragment.baseline()) };
-        Utf8View view { text.code_points().substring_view(fragment.start(), fragment.length()) };
-
-        auto& scaled_font = fragment.layout_node().scaled_font(context);
-
-        painter.draw_text_run(baseline_start.to_type<int>(), view, scaled_font, text_node.computed_values().color(), fragment_absolute_device_rect.to_type<int>());
+        auto const& scaled_font = fragment.layout_node().scaled_font(context);
+        Vector<Gfx::DrawGlyphOrEmoji> scaled_glyph_run;
+        scaled_glyph_run.ensure_capacity(fragment.glyph_run().size());
+        for (auto glyph : fragment.glyph_run()) {
+            glyph.visit([&](auto& glyph) {
+                glyph.font = &scaled_font;
+                glyph.position = glyph.position.scaled(context.device_pixels_per_css_pixel());
+            });
+            scaled_glyph_run.append(move(glyph));
+        }
+        painter.draw_text_run(baseline_start.to_type<int>(), scaled_glyph_run, text_node.computed_values().color(), fragment_absolute_device_rect.to_type<int>());
 
         auto selection_rect = context.enclosing_device_rect(fragment.selection_rect(text_node.font())).to_type<int>();
         if (!selection_rect.is_empty()) {
             painter.fill_rect(selection_rect, CSS::SystemColor::highlight());
             RecordingPainterStateSaver saver(painter);
             painter.add_clip_rect(selection_rect);
-            painter.draw_text_run(baseline_start.to_type<int>(), view, scaled_font, CSS::SystemColor::highlight_text(), fragment_absolute_device_rect.to_type<int>());
+            painter.draw_text_run(baseline_start.to_type<int>(), fragment.glyph_run(), CSS::SystemColor::highlight_text(), fragment_absolute_device_rect.to_type<int>());
         }
 
         paint_text_decoration(context, text_node, fragment);

Userland/Libraries/LibGfx/Painter.cpp
@@ -1422,7 +1422,7 @@ void Painter::draw_glyph_or_emoji(FloatPoint point, Utf8CodePointIterator& it, F
         draw_glyph(glyph.position, glyph.code_point, *glyph.font, color);
     } else {
         auto& emoji = draw_glyph_or_emoji.get<DrawEmoji>();
-        draw_emoji(emoji.position, *emoji.emoji, *emoji.font);
+        draw_emoji(emoji.position.to_type<int>(), *emoji.emoji, *emoji.font);
     }
 }
 
@@ -2445,7 +2445,7 @@ void Painter::draw_text_run(FloatPoint baseline_start, Utf8View const& string, F
             draw_glyph(glyph.position, glyph.code_point, *glyph.font, color);
         } else {
             auto& emoji = glyph_or_emoji.get<DrawEmoji>();
-            draw_emoji(emoji.position, *emoji.emoji, *emoji.font);
+            draw_emoji(emoji.position.to_type<int>(), *emoji.emoji, *emoji.font);
         }
     });
 }

Userland/Libraries/LibGfx/TextLayout.cpp
@@ -231,7 +231,7 @@ DrawGlyphOrEmoji prepare_draw_glyph_or_emoji(FloatPoint point, Utf8CodePointIter
     // If we didn't find a text glyph, or have an emoji variation selector or regional indicator, try to draw an emoji glyph.
     if (auto const* emoji = Emoji::emoji_for_code_point_iterator(it)) {
         return DrawEmoji {
-            .position = point.to_type<int>(),
+            .position = point,
             .emoji = emoji,
             .font = &font,
         };
@@ -253,17 +253,6 @@ DrawGlyphOrEmoji prepare_draw_glyph_or_emoji(FloatPoint point, Utf8CodePointIter
         .code_point = 0xFFFD,
         .font = &font,
     };
-}
-
-Vector<DrawGlyphOrEmoji> get_glyph_run(FloatPoint baseline_start, Utf8View const& string, Font const& font, IncludeLeftBearing include_left_bearing)
-{
-    Vector<DrawGlyphOrEmoji> glyphs_or_emojis;
-    for_each_glyph_position(
-        baseline_start, string, font, [&](auto glyph_or_emoji) {
-            glyphs_or_emojis.append(glyph_or_emoji);
-        },
-        include_left_bearing);
-    return glyphs_or_emojis;
 }
 
 }

Userland/Libraries/LibGfx/TextLayout.h
@@ -82,7 +82,7 @@ struct DrawGlyph {
 };
 
 struct DrawEmoji {
-    IntPoint position;
+    FloatPoint position;
     Gfx::Bitmap const* emoji;
     Font const* font;
 };
@@ -92,7 +92,7 @@ using DrawGlyphOrEmoji = Variant<DrawGlyph, DrawEmoji>;
 Variant<DrawGlyph, DrawEmoji> prepare_draw_glyph_or_emoji(FloatPoint point, Utf8CodePointIterator& it, Font const& font);
 
 template<typename Callback>
-void for_each_glyph_position(FloatPoint baseline_start, Utf8View string, Font const& font, Callback callback, IncludeLeftBearing include_left_bearing = IncludeLeftBearing::No)
+void for_each_glyph_position(FloatPoint baseline_start, Utf8View string, Font const& font, Callback callback, IncludeLeftBearing include_left_bearing = IncludeLeftBearing::No, Optional<float&> width = {})
 {
     float space_width = font.glyph_width(' ') + font.glyph_spacing();
 
@@ -127,8 +127,9 @@ void for_each_glyph_position(FloatPoint baseline_start, Utf8View string, Font co
         point.translate_by(glyph_width, 0);
         last_code_point = code_point;
     }
-}
 
-Vector<DrawGlyphOrEmoji> get_glyph_run(FloatPoint baseline_start, Utf8View const& string, Font const& font, IncludeLeftBearing include_left_bearing = IncludeLeftBearing::No);
+    if (width.has_value())
+        *width = point.x() - font.glyph_spacing();
+}
 
 }

Userland/Libraries/LibWeb/Layout/InlineFormattingContext.cpp
@@ -326,7 +326,8 @@ void InlineFormattingContext::generate_line_boxes(LayoutMode layout_mode)
                 item.margin_start,
                 item.margin_end,
                 item.width,
-                text_node.line_height());
+                text_node.line_height(),
+                item.glyph_run);
             break;
         }
         }

Userland/Libraries/LibWeb/Layout/InlineLevelIterator.cpp
@@ -187,12 +187,19 @@ Optional<InlineLevelIterator::Item> InlineLevelIterator::next_without_lookahead(
             };
         }
 
-        CSSPixels chunk_width;
+        Vector<Gfx::DrawGlyphOrEmoji> glyph_run;
+        float glyph_run_width = 0;
+        Gfx::for_each_glyph_position(
+            { 0, 0 }, chunk.view, text_node.font(), [&](Gfx::DrawGlyphOrEmoji const& glyph_or_emoji) {
+                glyph_run.append(glyph_or_emoji);
+                return IterationDecision::Continue;
+            },
+            Gfx::IncludeLeftBearing::No, glyph_run_width);
 
-        if (m_text_node_context->is_last_chunk)
-            chunk_width = CSSPixels::nearest_value_for(text_node.font().width(chunk.view));
-        else
-            chunk_width = CSSPixels::nearest_value_for(text_node.font().width(chunk.view) + text_node.font().glyph_spacing());
+        if (!m_text_node_context->is_last_chunk)
+            glyph_run_width += text_node.font().glyph_spacing();
+
+        CSSPixels chunk_width = CSSPixels::nearest_value_for(glyph_run_width);
 
         // NOTE: We never consider `content: ""` to be collapsible whitespace.
         bool is_generated_empty_string = text_node.is_generated() && chunk.length == 0;
@@ -200,6 +207,7 @@ Optional<InlineLevelIterator::Item> InlineLevelIterator::next_without_lookahead(
         Item item {
             .type = Item::Type::Text,
             .node = &text_node,
+            .glyph_run = move(glyph_run),
             .offset_in_node = chunk.start,
             .length_in_node = chunk.length,
             .width = chunk_width,

commit de71645d9fca6370a084238a11e503fc68a0f395

LibWeb/Painting: Use scaled font for selected text
Fixes regression introduced in:
https://github.com/SerenityOS/serenity/commit/42aacef1d5c00b7c00cbc86c5fc259b0fcb0cd70

Userland/Libraries/LibWeb/Painting/PaintableBox.cpp
@@ -662,7 +662,7 @@ static void paint_text_fragment(PaintContext& context, Layout::TextNode const& t
             painter.fill_rect(selection_rect, CSS::SystemColor::highlight());
             RecordingPainterStateSaver saver(painter);
             painter.add_clip_rect(selection_rect);
-            painter.draw_text_run(baseline_start.to_type<int>(), fragment.glyph_run(), CSS::SystemColor::highlight_text(), fragment_absolute_device_rect.to_type<int>());
+            painter.draw_text_run(baseline_start.to_type<int>(), scaled_glyph_run, CSS::SystemColor::highlight_text(), fragment_absolute_device_rect.to_type<int>());
         }
 
         paint_text_decoration(context, text_node, fragment);