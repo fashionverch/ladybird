fashionverch/ladybird

commit 060a6e8f2d072c507783403faa7f04b33cba39ab

LibWeb: Schedule repainting from EventLoop::process()
In this change, updating layout and painting are moved to the EventLoop
processing steps. This modification allows the addition of resize
observation dispatching that needs to happen in event loop processing
steps and must occur in the following order relative to layout and
painting:

1. Update layout.
2. Gather and broadcast resize observations.
3. Paint.

Userland/Libraries/LibWeb/HTML/EventLoop/EventLoop.cpp
@@ -15,6 +15,7 @@
 #include <LibWeb/HTML/Window.h>
 #include <LibWeb/HighResolutionTime/Performance.h>
 #include <LibWeb/HighResolutionTime/TimeOrigin.h>
+#include <LibWeb/Page/Page.h>
 #include <LibWeb/Platform/EventLoopPlugin.h>
 #include <LibWeb/Platform/Timer.h>
 
@@ -213,7 +214,12 @@ void EventLoop::process()
 
     // FIXME:     15. Invoke the mark paint timing algorithm for each Document object in docs.
 
-    // FIXME:     16. For each fully active Document in docs, update the rendering or user interface of that Document and its browsing context to reflect the current state.
+    // 16. For each fully active Document in docs, update the rendering or user interface of that Document and its browsing context to reflect the current state.
+    for_each_fully_active_document_in_docs([&](DOM::Document& document) {
+        auto* browsing_context = document.browsing_context();
+        auto& page = browsing_context->page();
+        page.client().schedule_repaint();
+    });
 
     // 13. If all of the following are true
     // - this is a window event loop

Userland/Services/WebContent/PageClient.cpp
@@ -222,12 +222,12 @@ void PageClient::paint(Web::DevicePixelRect const& content_rect, Gfx::Bitmap& ta
 void PageClient::set_viewport_rect(Web::DevicePixelRect const& rect)
 {
     page().top_level_traversable()->set_viewport_rect(page().device_to_css_rect(rect));
-    schedule_repaint();
+    Web::HTML::main_thread_event_loop().schedule();
 }
 
 void PageClient::page_did_invalidate(Web::CSSPixelRect const&)
 {
-    schedule_repaint();
+    Web::HTML::main_thread_event_loop().schedule();
 }
 
 void PageClient::page_did_request_cursor_change(Gfx::StandardCursor cursor)

Userland/Libraries/LibWeb/Page/Page.h
@@ -302,6 +302,8 @@ public:
     virtual void inspector_did_request_dom_tree_context_menu([[maybe_unused]] i32 node_id, [[maybe_unused]] CSSPixelPoint position, [[maybe_unused]] String const& type, [[maybe_unused]] Optional<String> const& tag, [[maybe_unused]] Optional<String> const& attribute_name, [[maybe_unused]] Optional<String> const& attribute_value) { }
     virtual void inspector_did_execute_console_script([[maybe_unused]] String const& script) { }
 
+    virtual void schedule_repaint() = 0;
+
 protected:
     virtual ~PageClient() = default;
 };

Userland/Libraries/LibWeb/SVG/SVGDecodedImageData.cpp
@@ -48,6 +48,7 @@ public:
     virtual CSS::PreferredColorScheme preferred_color_scheme() const override { return m_host_page.client().preferred_color_scheme(); }
     virtual void request_file(FileRequest) override { }
     virtual void paint(DevicePixelRect const&, Gfx::Bitmap&, Web::PaintOptions = {}) override { }
+    virtual void schedule_repaint() override { }
 
 private:
     explicit SVGPageClient(Page& host_page)

Userland/Services/WebContent/PageClient.h
@@ -28,7 +28,7 @@ public:
 
     static void set_use_gpu_painter();
 
-    void schedule_repaint();
+    virtual void schedule_repaint() override;
 
     virtual Web::Page& page() override { return *m_page; }
     virtual Web::Page const& page() const override { return *m_page; }

Userland/Services/WebWorker/PageHost.h
@@ -30,6 +30,7 @@ public:
     virtual Web::CSS::PreferredColorScheme preferred_color_scheme() const override;
     virtual void paint(Web::DevicePixelRect const&, Gfx::Bitmap&, Web::PaintOptions = {}) override;
     virtual void request_file(Web::FileRequest) override;
+    virtual void schedule_repaint() override {};
 
 private:
     explicit PageHost(ConnectionFromClient&);

commit 6ea67d62c810d5caebb4b458a46ff423679b9788

LibWeb: Paint page only if something that requires repaint happened
Resolves a performance regression from
060a6e8f2d072c507783403faa7f04b33cba39ab, where moving paint scheduling
to `EventLoop::process()` led to unnecessary repaints.

This update introduces a flag to trigger repaints only when necessary,
addressing the issue where repaints previously occurred with each event
loop process, irrespective of actual changes.

Userland/Libraries/LibWeb/HTML/EventLoop/EventLoop.cpp
@@ -256,9 +256,11 @@ void EventLoop::process()
 
     // 16. For each fully active Document in docs, update the rendering or user interface of that Document and its browsing context to reflect the current state.
     for_each_fully_active_document_in_docs([&](DOM::Document& document) {
-        auto* browsing_context = document.browsing_context();
-        auto& page = browsing_context->page();
-        page.client().schedule_repaint();
+        if (document.navigable() && document.navigable()->needs_repaint()) {
+            auto* browsing_context = document.browsing_context();
+            auto& page = browsing_context->page();
+            page.client().schedule_repaint();
+        }
     });
 
     // 13. If all of the following are true

Userland/Libraries/LibWeb/HTML/Navigable.cpp
@@ -1980,12 +1980,14 @@ void Navigable::set_viewport_rect(CSSPixelRect const& rect)
             document->set_needs_layout();
         }
         did_change = true;
+        m_needs_repaint = true;
     }
 
     if (m_viewport_scroll_offset != rect.location()) {
         m_viewport_scroll_offset = rect.location();
         scroll_offset_did_change();
         did_change = true;
+        m_needs_repaint = true;
     }
 
     if (did_change && active_document()) {
@@ -2035,6 +2037,8 @@ void Navigable::set_needs_display(CSSPixelRect const& rect)
 {
     // FIXME: Ignore updates outside the visible viewport rect.
     //        This requires accounting for fixed-position elements in the input rect, which we don't do yet.
+
+    m_needs_repaint = true;
 
     if (is<TraversableNavigable>(*this)) {
         static_cast<TraversableNavigable*>(this)->page().client().page_did_invalidate(to_top_level_rect(rect));
@@ -2128,6 +2132,8 @@ void Navigable::paint(Painting::RecordingPainter& recording_painter, PaintConfig
         }
         recording_painter.commands_list().apply_scroll_offsets(scroll_offsets_by_frame_id);
     }
+
+    m_needs_repaint = false;
 }
 
 // https://html.spec.whatwg.org/multipage/browsing-the-web.html#event-uni

Userland/Libraries/LibWeb/HTML/Navigable.h
@@ -173,6 +173,8 @@ public:
 
     [[nodiscard]] TargetSnapshotParams snapshot_target_snapshot_params();
 
+    [[nodiscard]] bool needs_repaint() const { return m_needs_repaint; }
+
     struct PaintConfig {
         bool paint_overlay { false };
         bool should_show_line_box_borders { false };
@@ -221,6 +223,8 @@ private:
 
     CSSPixelSize m_size;
     CSSPixelPoint m_viewport_scroll_offset;
+
+    bool m_needs_repaint { false };
 };
 
 HashTable<Navigable*>& all_navigables();

Userland/Services/WebContent/PageClient.cpp
@@ -222,7 +222,6 @@ void PageClient::paint(Web::DevicePixelRect const& content_rect, Gfx::Bitmap& ta
 void PageClient::set_viewport_rect(Web::DevicePixelRect const& rect)
 {
     page().top_level_traversable()->set_viewport_rect(page().device_to_css_rect(rect));
-    Web::HTML::main_thread_event_loop().schedule();
 }
 
 void PageClient::page_did_invalidate(Web::CSSPixelRect const&)