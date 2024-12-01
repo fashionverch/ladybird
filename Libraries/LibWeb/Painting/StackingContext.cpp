fashionverch/ladybird

commit 8110f6a7ae3ca623255dd464d52a6f0a131e6f39

LibWeb: Use separate restore() for each ApplyFilters display list item
ApplyFilter internally calls canvas.saveLayer() which requires a
matching canvas.restore() to be called.

Fixes painting on https://supabase.com/ regressed by
9f5ee26372df3e094358974a3898c1b68d5d57be

Libraries/LibWeb/Painting/StackingContext.cpp
@@ -327,7 +327,11 @@ void StackingContext::paint(PaintContext& context) const
         context.display_list_recorder().push_scroll_frame_id(*paintable_box().scroll_frame_id());
     }
     context.display_list_recorder().push_stacking_context(push_stacking_context_params);
-    context.display_list_recorder().apply_filters(paintable_box().computed_values().filter());
+
+    auto const& filter = computed_values.filter();
+    if (!filter.is_none()) {
+        context.display_list_recorder().apply_filters(paintable_box().computed_values().filter());
+    }
 
     if (auto mask_image = computed_values.mask_image()) {
         auto mask_display_list = DisplayList::create();
@@ -346,6 +350,10 @@ void StackingContext::paint(PaintContext& context) const
             auto masking_area_rect = context.enclosing_device_rect(*masking_area).to_type<int>();
             context.display_list_recorder().apply_mask_bitmap(masking_area_rect.location(), mask_bitmap.release_nonnull(), *paintable_box().get_mask_type());
         }
+    }
+
+    if (!filter.is_none()) {
+        context.display_list_recorder().restore();
     }
 
     paint_internal(context);

Libraries/LibWeb/Painting/SVGSVGPaintable.cpp
@@ -55,9 +55,9 @@ void SVGSVGPaintable::paint_svg_box(PaintContext& context, PaintableBox const& s
 {
     auto const& computed_values = svg_box.computed_values();
 
-    auto filters = svg_box.computed_values().filter();
+    auto const& filter = svg_box.computed_values().filter();
     auto masking_area = svg_box.get_masking_area();
-    auto needs_to_save_state = computed_values.opacity() < 1 || svg_box.has_css_transform() || svg_box.get_masking_area().has_value() || !filters.is_none();
+    auto needs_to_save_state = computed_values.opacity() < 1 || svg_box.has_css_transform() || svg_box.get_masking_area().has_value();
 
     if (needs_to_save_state) {
         context.display_list_recorder().save();
@@ -67,7 +67,9 @@ void SVGSVGPaintable::paint_svg_box(PaintContext& context, PaintableBox const& s
         context.display_list_recorder().apply_opacity(computed_values.opacity());
     }
 
-    context.display_list_recorder().apply_filters(filters);
+    if (!filter.is_none()) {
+        context.display_list_recorder().apply_filters(filter);
+    }
 
     if (svg_box.has_css_transform()) {
         auto transform_matrix = svg_box.transform();
@@ -92,6 +94,10 @@ void SVGSVGPaintable::paint_svg_box(PaintContext& context, PaintableBox const& s
     svg_box.after_paint(context, PaintPhase::Foreground);
 
     paint_descendants(context, svg_box, phase);
+
+    if (!filter.is_none()) {
+        context.display_list_recorder().restore();
+    }
 
     if (needs_to_save_state) {
         context.display_list_recorder().restore();

commit 9f655672277724424a4c6d2c0d5d816afe453b77

LibWeb: Fix CSS filters painting
restore() corresponding to ApplyFilters should be called after stacking
context content is painted, not before.

Fixes regression introduced in 8110f6a7ae3ca623255dd464d52a6f0a131e6f39

Libraries/LibWeb/Painting/StackingContext.cpp
@@ -352,11 +352,12 @@ void StackingContext::paint(PaintContext& context) const
         }
     }
 
+    paint_internal(context);
+
     if (!filter.is_none()) {
         context.display_list_recorder().restore();
     }
 
-    paint_internal(context);
     context.display_list_recorder().pop_stacking_context();
     if (paintable_box().scroll_frame_id().has_value()) {
         context.display_list_recorder().pop_scroll_frame_id();