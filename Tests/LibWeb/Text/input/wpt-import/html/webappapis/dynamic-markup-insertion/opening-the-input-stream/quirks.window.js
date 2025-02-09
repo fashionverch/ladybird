fashionverch/ladybird

commit 177358f67f707032c506adcd75a1173e196937ce

LibWeb: Don't end parsing after reaching the insertion point

Libraries/LibWeb/HTML/Parser/HTMLParser.cpp
@@ -191,10 +191,6 @@ void HTMLParser::run(HTMLTokenizer::StopAtInsertionPoint stop_at_insertion_point
     m_stop_parsing = false;
 
     for (;;) {
-        // FIXME: Find a better way to say that we come from Document::close() and want to process EOF.
-        if (!m_tokenizer.is_eof_inserted() && m_tokenizer.is_insertion_point_reached())
-            break;
-
         auto optional_token = m_tokenizer.next_token(stop_at_insertion_point);
         if (!optional_token.has_value())
             break;

Libraries/LibWeb/DOM/Document.cpp
@@ -708,14 +708,14 @@ WebIDL::ExceptionOr<void> Document::close()
     if (!m_parser)
         return {};
 
-    // FIXME: 4. Insert an explicit "EOF" character at the end of the parser's input stream.
+    // 4. Insert an explicit "EOF" character at the end of the parser's input stream.
     m_parser->tokenizer().insert_eof();
 
     // 5. If there is a pending parsing-blocking script, then return.
     if (pending_parsing_blocking_script())
         return {};
 
-    // FIXME: 6. Run the tokenizer, processing resulting tokens as they are emitted, and stopping when the tokenizer reaches the explicit "EOF" character or spins the event loop.
+    // 6. Run the tokenizer, processing resulting tokens as they are emitted, and stopping when the tokenizer reaches the explicit "EOF" character or spins the event loop.
     m_parser->run();
 
     // AD-HOC: This ensures that a load event is fired if the node navigable's container is an iframe.

Tests/LibWeb/Text/input/wpt-import/html/semantics/scripting-1/the-script-element/execution-timing/149.html
@@ -1,0 +1,59 @@
+<!DOCTYPE html>
+<html><head>
+  <title>scheduler: event/for attribute on script</title>
+  <script src="../../../../../resources/testharness.js"></script>
+  <script src="../../../../../resources/testharnessreport.js"></script>
+  <script src="testlib/testlib.js"></script>
+</head>
+<body>
+<div id="log"></div>
+<script>
+
+attributes = [
+  {for:"window", event:"onload()", expect:true},
+  {for:"window", event:"onload", expect:true},
+  {for:"  WINdow\t\n", event:"ONload\t\n", expect:true},
+  {for:"window", event:"load", expect:false},
+  {for:"window", event:"onpageshow", expect:false},
+  {for:"document", event:"onload", expect:false},
+]
+
+function test_maker(array_name) {
+  return function(x, i) {
+    var title = "for='" + x.for + "' event='" + x.event + "' " + array_name.replace("_", " ") + " " + (x.expect ? "executes immediately" : "does not execute");
+    script_content = "var d =" + array_name + "[" + i  + "];"
+    script_content += x.expect?"d[1].step(function() {d[3] = true});":"d[1].step(function() {assert_unreached()});"
+    return [x, async_test(title), script_content, false];
+  }
+}
+
+parser_inserted = attributes.map(test_maker("parser_inserted"));
+dom_inserted = attributes.map(test_maker("dom_inserted"));
+
+parser_inserted.forEach(function(x) {
+  var d = x[0];
+  document.write("<script for='" + d.for + "' event='" + d.event + "'>" + x[2] + "<\/script>");
+});
+
+dom_inserted.forEach(function(x) {
+  var d = x[0];
+  var s = document.createElement("script");
+  s.setAttribute("event", d.event);
+  s.setAttribute("for", d.for);
+  s.textContent = x[2];
+  document.body.appendChild(s);
+});
+</script>
+
+<script>
+var all_tests = parser_inserted.concat(dom_inserted);
+
+all_tests.filter(function(x) {return x[0]["expect"]}).forEach(function(x) {var t = x[1]; t.step(function() {assert_true(x[3])});})
+
+onload = function() {
+  all_tests.forEach(function(x) {var t = x[1]; t.step(function() {t.done()})});
+}
+</script>
+
+</body>
+</html>

Tests/LibWeb/Text/input/wpt-import/html/semantics/scripting-1/the-script-element/execution-timing/testlib/testlib.js
@@ -1,0 +1,43 @@
+/*
+* Utility functions for script scheduler test
+*/
+(function(){ /* namespace hiding local variables like arOrderOfAllEvents from global scope */
+    window.testlib = {};
+    window.eventOrder = [];
+    var arNumberOfScriptsParsedPerEvent=[];
+    window.log = function (str){
+        eventOrder.push(str);
+        arNumberOfScriptsParsedPerEvent.push(document.getElementsByTagName('script').length);
+    }
+
+    window.testlib.addScript = function(source, attributes, parent, firstInParent,funcPrepare) {
+        try{
+            parent = parent||document.body;
+            var script = document.createElement('script');
+            if(funcPrepare) {
+                funcPrepare(script);
+            }
+            if(source)script.appendChild( document.createTextNode(source) );
+            for( var name in attributes){
+                if(/^on/i.test(name)) {
+                    script[name] = attributes[name];
+                } else {
+                    script.setAttribute(name, attributes[name]);
+                }
+            }
+            if (firstInParent && parent.firstChild) {
+                parent.insertBefore(script, parent.firstChild);
+            } else {
+                parent.appendChild(script);
+            }
+        } catch(e) {
+            log('ERROR when adding script to DOM!');
+            alert(e);
+        }
+        return script;
+    }
+
+    window.testlib.urlParam = function(relativeURL) {
+        return location.href.replace( /\d*\.html$/, relativeURL);
+    }
+})();

commit 5ae4ebf840da1ce25bf60f9b29bc3549e2cfc0f3

LibWeb/HTML: Bail from HTML parsing when EOF hit on document.close
This fixes a crash in the included test that regressed in 177358f,
and is hit by the following HTML:

```html
<body></body>
<script>
  const frame = document.body.appendChild(document.createElement("iframe"));
  frame.contentDocument.open();
  const child = frame.contentDocument.createElement("html")
  const html = frame.contentDocument.appendChild(child);
  frame.contentDocument.close();
</script>
```

I am not 100% sure this is fully the correct fix and there are other
cases which would not work properly. But it's definitely an improvement
to make the confuisingly named 'insert_an_eof' function of the tokenizer
actually do something.

Libraries/LibWeb/HTML/Parser/HTMLParser.cpp
@@ -199,6 +199,9 @@ void HTMLParser::run(HTMLTokenizer::StopAtInsertionPoint stop_at_insertion_point
 
         dbgln_if(HTML_PARSER_DEBUG, "[{}] {}", insertion_mode_name(), token.to_string());
 
+        if (token.is_end_of_file() && m_tokenizer.is_eof_inserted())
+            break;
+
         // https://html.spec.whatwg.org/multipage/parsing.html#tree-construction-dispatcher
         // As each token is emitted from the tokenizer, the user agent must follow the appropriate steps from the following list, known as the tree construction dispatcher:
         if (m_stack_of_open_elements.is_empty()

Tests/LibWeb/Text/input/wpt-import/html/webappapis/dynamic-markup-insertion/opening-the-input-stream/quirks.window.html
@@ -1,0 +1,8 @@
+<!doctype html>
+<meta charset=utf-8>
+
+<script src="../../../../resources/testharness.js"></script>
+<script src="../../../../resources/testharnessreport.js"></script>
+
+<div id=log></div>
+<script src="../../../../html/webappapis/dynamic-markup-insertion/opening-the-input-stream/quirks.window.js"></script>

Tests/LibWeb/Text/input/wpt-import/html/webappapis/dynamic-markup-insertion/opening-the-input-stream/quirks.window.js
@@ -1,0 +1,74 @@
+test(t => {
+  const frame = document.body.appendChild(document.createElement("iframe"));
+  t.add_cleanup(() => frame.contentDocument.close());
+  assert_equals(frame.contentDocument.compatMode, "BackCompat");
+  frame.contentDocument.open();
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+  frame.contentDocument.close();
+  assert_equals(frame.contentDocument.compatMode, "BackCompat");
+}, "document.open() sets document to no-quirks mode (write no doctype)");
+
+test(t => {
+  const frame = document.body.appendChild(document.createElement("iframe"));
+  t.add_cleanup(() => frame.contentDocument.close());
+  assert_equals(frame.contentDocument.compatMode, "BackCompat");
+  frame.contentDocument.open();
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+  frame.contentDocument.write("<!doctype html public");
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+  frame.contentDocument.write(" \"-//IETF//DTD HTML 3//\"");
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+  frame.contentDocument.write(">");
+  assert_equals(frame.contentDocument.compatMode, "BackCompat");
+  frame.contentDocument.close();
+  assert_equals(frame.contentDocument.compatMode, "BackCompat");
+}, "document.open() sets document to no-quirks mode (write old doctype)");
+
+test(t => {
+  const frame = document.body.appendChild(document.createElement("iframe"));
+  t.add_cleanup(() => frame.contentDocument.close());
+  assert_equals(frame.contentDocument.compatMode, "BackCompat");
+  frame.contentDocument.open();
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+  frame.contentDocument.write("<!doctype html");
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+  frame.contentDocument.write(">");
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+  frame.contentDocument.close();
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+}, "document.open() sets document to no-quirks mode (write new doctype)");
+
+// This tests the document.open() call in fact sets the document to no-quirks
+// mode, not limited-quirks mode. It is derived from
+// quirks/blocks-ignore-line-height.html in WPT, as there is no direct way to
+// distinguish between a no-quirks document and a limited-quirks document. It
+// assumes that the user agent passes the linked test, which at the time of
+// writing is all major web browsers.
+test(t => {
+  const frame = document.body.appendChild(document.createElement("iframe"));
+  t.add_cleanup(() => frame.contentDocument.close());
+  assert_equals(frame.contentDocument.compatMode, "BackCompat");
+  frame.contentDocument.open();
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+
+  // Create the DOM tree manually rather than going through document.write() to
+  // bypass the parser, which resets the document mode.
+  const html = frame.contentDocument.appendChild(frame.contentDocument.createElement("html"));
+  const body = html.appendChild(frame.contentDocument.createElement("body"));
+  assert_equals(frame.contentDocument.body, body);
+  body.innerHTML = `
+    <style>#ref { display:block }</style>
+    <div id=test><font size=1>x</font></div>
+    <font id=ref size=1>x</font>
+    <div id=s_ref>x</div>
+  `;
+  assert_equals(frame.contentDocument.compatMode, "CSS1Compat");
+
+  const idTest = frame.contentDocument.getElementById("test");
+  const idRef = frame.contentDocument.getElementById("ref");
+  const idSRef = frame.contentDocument.getElementById("s_ref");
+  assert_equals(frame.contentWindow.getComputedStyle(idTest).height,
+                frame.contentWindow.getComputedStyle(idSRef).height);
+  assert_not_equals(frame.contentWindow.getComputedStyle(idTest).height,
+                    frame.contentWindow.getComputedStyle(idRef).height);
+}, "document.open() sets document to no-quirks mode, not limited-quirks mode");