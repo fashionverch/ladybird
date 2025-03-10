fashionverch/ladybird

commit 9d7b3f04c01746b82faf07de07b7b1c74aede999

LibWeb/HTML: Scroll to the fragment before loading the document
Otherwise nowhere ends up scrolling to the fragment specified by the
fragment in document's URL. This fixes ladybird scrolling to the
correct location in the document when navigating to a link that
has a fragment, e.g:

https://html.spec.whatwg.org/multipage/browsing-the-web.html#try-to-scroll-to-the-fragment

As well as use of the :target selector.

Libraries/LibWeb/HTML/Parser/HTMLParser.cpp
@@ -310,6 +310,9 @@ void HTMLParser::the_end(GC::Ref<DOM::Document> document, GC::Ptr<HTMLParser> pa
         (void)document->scripts_to_execute_when_parsing_has_finished().take_first();
     }
 
+    // FIXME: Spec bug: https://github.com/whatwg/html/issues/10914
+    document->scroll_to_the_fragment();
+
     // 6. Queue a global task on the DOM manipulation task source given the Document's relevant global object to run the following substeps:
     queue_global_task(HTML::Task::Source::DOMManipulation, *document, GC::create_function(heap, [document] {
         // 1. Set the Document's load timing info's DOM content loaded event start time to the current high resolution time given the Document's relevant global object.

Tests/LibWeb/Text/input/wpt-import/url/data-uri-fragment.html
@@ -1,0 +1,34 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>Data URI parsing of fragments</title>
+<link rel="help" href="https://url.spec.whatwg.org/">
+<meta name="assert" content="Fragments should not be included as part of a data URI's body">
+
+<script src="../resources/testharness.js"></script>
+<script src="../resources/testharnessreport.js"></script>
+
+<iframe id="iframe"></iframe>
+
+<script>
+const IFRAME_DATA_SRC = `data:text/html,
+    <style>:target { color: green; }<\/style>
+    <script>window.addEventListener('load', function() {
+      const data = {
+        foo_matches_target_selector: document.getElementById('foo').matches(':target'),
+        body_html: document.body.innerHTML,
+      };
+      parent.postMessage(data, '*');
+    });<\/script>
+    <p id="foo">This should be the only visible text.</p>#foo`.replace('\n', '');
+
+async_test(function(t) {
+  window.addEventListener("message", t.step_func_done(function(event) {
+    assert_true(event.data.foo_matches_target_selector);
+    assert_equals(event.data.body_html,
+        '<p id="foo">This should be the only visible text.</p>');
+  }));
+
+  const iframe = document.getElementById("iframe");
+  iframe.src = IFRAME_DATA_SRC;
+});
+</script>

commit 97f68618e69d9197c4a18094d960fbce5f9fd70f

LibWeb: Respect scroll position set by script during page load
When setting scroll position during page load we need to consider
whether we actually have a fragment to scroll to. A script may already
have run at that point and may already have set a scroll position.

If there is an actual fragment to scroll to, it is fine to scroll to
that fragment, since it should take precedence. If we don't have a
fragment however, we should not unnecessarily overwrite the scroll
position set by the script back to (0, 0).

Since this problem is caused by a spec bug, I have tested the behavior
in the three major browsers engines. Unfortunately they do not agree
fully with each other. If there is no fragment at all (e.g. `foo.html`),
all browsers will respect the scroll position set by the script. If
there is a fragment (e.g. `foo.html#bar`), all browsers will set the
scroll position to the fragment element and ignore the one set by
script. However, when the fragment is empty (e.g. `foo.html#`), then
Blink and WebKit will set scroll position to the fragment, while Gecko
will set scroll position from script. Since all of this is ad-hoc
behavior anyway, I simply implemented the Blink/WebKit behavior because
of the majority vote for now.

This fixes a regression introduced in 9d7b3f04c0.

Libraries/LibWeb/HTML/Parser/HTMLParser.cpp
@@ -18,6 +18,7 @@
 #include <LibWeb/DOM/Comment.h>
 #include <LibWeb/DOM/Document.h>
 #include <LibWeb/DOM/DocumentType.h>
+#include <LibWeb/DOM/Element.h>
 #include <LibWeb/DOM/ElementFactory.h>
 #include <LibWeb/DOM/Event.h>
 #include <LibWeb/DOM/ProcessingInstruction.h>
@@ -321,8 +322,14 @@ void HTMLParser::the_end(GC::Ref<DOM::Document> document, GC::Ptr<HTMLParser> pa
         (void)document->scripts_to_execute_when_parsing_has_finished().take_first();
     }
 
-    // FIXME: Spec bug: https://github.com/whatwg/html/issues/10914
-    document->scroll_to_the_fragment();
+    // AD-HOC: We need to scroll to the fragment on page load somewhere.
+    // But a script that ran in step 5 above may have scrolled the page already,
+    // so only do this if there is an actual fragment to avoid resetting the scroll position unexpectedly.
+    // Spec bug: https://github.com/whatwg/html/issues/10914
+    auto indicated_part = document->determine_the_indicated_part();
+    if (indicated_part.has<DOM::Element*>() && indicated_part.get<DOM::Element*>() != nullptr) {
+        document->scroll_to_the_fragment();
+    }
 
     // 6. Queue a global task on the DOM manipulation task source given the Document's relevant global object to run the following substeps:
     queue_global_task(HTML::Task::Source::DOMManipulation, *document, GC::create_function(heap, [document] {

Tests/LibWeb/Text/input/scroll-during-page-load.html
@@ -1,0 +1,12 @@
+<!DOCTYPE html>
+<script src="include.js"></script>
+<style>
+div { height: 200vh; }
+</style>
+<div></div>
+<script>
+    window.scrollTo(0, 200);
+    test(() => {
+        println(`Scroll position: ${window.scrollY}px`);
+    });
+</script>